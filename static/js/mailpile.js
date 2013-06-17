c54ece069957f229a3827aa455b2a6a3a065a0ee
Smári McCarthy <smari@immi.is>
2013-06-17 04:46:21 +0000

    More search magic.

--- static/js/mailpile.js
+++ static/js/mailpile.js
@@ -1,4 +1,12 @@
 
+Number.prototype.pad = function(size){
+	// Unfortunate padding function....
+	if(typeof(size) !== "number"){size = 2;}
+	var s = String(this);
+	while (s.length < size) s = "0" + s;
+	return s;
+}
+
 
 function MailPile() {
 	this.msgcache = [];
@@ -25,16 +33,23 @@ MailPile.prototype.rescan = function() {}
 MailPile.prototype.gpgrecv = function() {}
 MailPile.prototype.search = function(q) {
 	var that = this;
+	$("#qbox").val(q);
 	this.json_get("search", {"q": q}, function(data) {
 		$("#results tbody").empty();
 		for (var i = 0; i < data.results.length; i++) {
+			msg_info = data.results[i].msg_info;
+			msg_tags = data.results[i].msg_tags;
+			d = new Date(parseInt(msg_info[4], 36)*1000)
+			zpymd = d.getFullYear() + "-" + (d.getMonth()+1).pad(2) + "-" + d.getDate().pad(2);
+			ymd = d.getFullYear() + "-" + (d.getMonth()+1) + "-" + d.getDate();
+			taghrefs = msg_tags.map(function(e){ return '<a onclick="mailpile.search(\'\\' + e + '\')">' + e + '</a>'}).join(" ");
 			tr = $('<tr class="result"></tr>');
 			tr.addClass((i%2==0)?"even":"odd");
-			tr.append('<td class="checkbox"></td>');
-			tr.append('<td class="from">' + data.results[i].msg_info[5] + '</td>');
-			tr.append('<td class="subject">' + data.results[i].msg_info[6] + '</td>');
-			tr.append('<td class="tags"></td>');
-			tr.append('<td class="date"></td>');
+			tr.append('<td class="checkbox"><input type="checkbox" name="msg_' + msg_info[0] + '"/></td>');
+			tr.append('<td class="from">' + msg_info[5] + '</td>');
+			tr.append('<td class="subject">' + msg_info[6] + '</td>');
+			tr.append('<td class="tags">' + taghrefs + '</td>');
+			tr.append('<td class="date"><a onclick="mailpile.search(\'date:' + ymd + '\');">' + zpymd + '</a></td>');
 			$("#results tbody").append(tr);
 		}
 		that.chatter(data.chatter);
