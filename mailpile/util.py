e176db4730f300a9984d5a3c707c9e74bd89cadd
Bjarni R. Einarsson <bre@klaki.net>
2013-09-24 19:40:42 +0000

    PEP8

--- mailpile/util.py
+++ mailpile/util.py
@@ -12,15 +12,24 @@ import sys
 import tempfile
 import threading
 import time
+import StringIO
+
+try:
+    import Image
+except:
+    Image = None
+
 
 global APPEND_FD_CACHE, APPEND_FD_CACHE_ORDER, APPEND_FD_CACHE_SIZE
 global WORD_REGEXP, STOPLIST, BORING_HEADERS, DEFAULT_PORT, QUITTING
+
 
 QUITTING = False
 
 DEFAULT_PORT = 33411
 
-WORD_REGEXP = re.compile('[^\s!@#$%^&*\(\)_+=\{\}\[\]:\"|;\'\\\<\>\?,\.\/\-]{2,}')
+WORD_REGEXP = re.compile('[^\s!@#$%^&*\(\)_+=\{\}\[\]'
+                         ':\"|;\'\\\<\>\?,\.\/\-]{2,}')
 
 STOPLIST = set(['an', 'and', 'are', 'as', 'at', 'by', 'for', 'from',
                 'has', 'http', 'in', 'is', 'it', 'mailto', 'og', 'or',
@@ -29,7 +38,6 @@ STOPLIST = set(['an', 'and', 'are', 'as', 'at', 'by', 'for', 'from',
 BORING_HEADERS = ('received', 'date',
                   'content-type', 'content-disposition', 'mime-version',
                   'dkim-signature', 'domainkey-signature', 'received-spf')
-
 
 
 class WorkerError(Exception):
@@ -51,152 +59,163 @@ class UrlRedirectException(Exception):
         self.url = url
 
 
-def b64c(b): return b.replace('\n', '').replace('=', '').replace('/', '_')
-def b64w(b): return b64c(b).replace('+', '-')
+def b64c(b):
+    return b.replace('\n', '').replace('=', '').replace('/', '_')
 
-import cgi
+
+def b64w(b):
+    return b64c(b).replace('+', '-')
+
 
 def escape_html(t):
-  """
-  Replace characters that have a special meaning in HTML
-  by their entity equivalents. Return the replaced
-  string.
+    """
+    Replace characters that have a special meaning in HTML
+    by their entity equivalents. Return the replaced
+    string.
 
-  >>> escape_html("Hello, Goodbye.")
-  'Hello, Goodbye.'
-  >>> escape_html("Hello<>World")
-  'Hello&lt;&gt;World'
-  >>> escape_html("<&>")
-  '&lt;&amp;&gt;'
+    >>> escape_html("Hello, Goodbye.")
+    'Hello, Goodbye.'
+    >>> escape_html("Hello<>World")
+    'Hello&lt;&gt;World'
+    >>> escape_html("<&>")
+    '&lt;&amp;&gt;'
 
-  Keyword arguments:
-  t -- The string to escape
-  """
-  return cgi.escape(t)
+    Keyword arguments:
+    t -- The string to escape
+    """
+    return cgi.escape(t)
+
 
 def sha1b64(s):
-  """
-  Apply the SHA1 hash algorithm to a string
-  and return the base64-encoded hash value
+    """
+    Apply the SHA1 hash algorithm to a string
+    and return the base64-encoded hash value
 
-  >>> sha1b64("Hello")
-  '9/+ei3uy4Jtwk1pdeF4MxdnQq/A=\\n'
+    >>> sha1b64("Hello")
+    '9/+ei3uy4Jtwk1pdeF4MxdnQq/A=\\n'
 
-  Keyword arguments:
-  s -- The string to hash
-  """
-  h = hashlib.sha1()
-  if type(s) == type(unicode()):
-    h.update(s.encode('utf-8'))
-  else:
-    h.update(s)
-  return h.digest().encode('base64')
+    Keyword arguments:
+    s -- The string to hash
+    """
+    h = hashlib.sha1()
+    if type(s) == type(unicode()):
+        h.update(s.encode('utf-8'))
+    else:
+        h.update(s)
+    return h.digest().encode('base64')
+
 
 def sha512b64(s):
-  """
-  Apply the SHA512 hash algorithm to a string
-  and return the base64-encoded hash value
+    """
+    Apply the SHA512 hash algorithm to a string
+    and return the base64-encoded hash value
 
-  >>> sha512b64("Hello")
-  'NhX4DJ0pPtdAJof5SyLVjlKbjMeRb4+sf933+9WvTPd309eVp6AKFr9+fz+5Vh7puq5IDan+ehh2\\nnnGIawPzFQ==\\n'
-  >>> sha512b64(u"Hello")
-  'NhX4DJ0pPtdAJof5SyLVjlKbjMeRb4+sf933+9WvTPd309eVp6AKFr9+fz+5Vh7puq5IDan+ehh2\\nnnGIawPzFQ==\\n'
+    >>> sha512b64("Hello")[:64]
+    'NhX4DJ0pPtdAJof5SyLVjlKbjMeRb4+sf933+9WvTPd309eVp6AKFr9+fz+5Vh7p'
+    >>> sha512b64(u"Hello")[:64]
+    'NhX4DJ0pPtdAJof5SyLVjlKbjMeRb4+sf933+9WvTPd309eVp6AKFr9+fz+5Vh7p'
 
-  Keyword arguments:
-  s -- The string to hash
-  """
-  h = hashlib.sha512()
-  if type(s) == type(unicode()):
-    h.update(s.encode('utf-8'))
-  else:
-    h.update(s)
-  return h.digest().encode('base64')
+    Keyword arguments:
+    s -- The string to hash
+    """
+    h = hashlib.sha512()
+    if type(s) == type(unicode()):
+        h.update(s.encode('utf-8'))
+    else:
+        h.update(s)
+    return h.digest().encode('base64')
+
 
 def strhash(s, length, obfuscate=None):
-  """
-  Create a hash of
+    """
+    Create a hash of
 
-  >>> strhash("Hello", 10)
-  'hello9_+ei'
-  >>> strhash("Goodbye", 5, obfuscate="mysalt")
-  'voxpj'
+    >>> strhash("Hello", 10)
+    'hello9_+ei'
+    >>> strhash("Goodbye", 5, obfuscate="mysalt")
+    'voxpj'
 
-  Keyword arguments:
-  s -- The string to be hashed
-  length -- The length of the hash to create.
-            Might be limited by the hash method
-  obfuscate -- None to disable SHA512 obfuscation,
-               or a salt to append to the string
-               before hashing
-  """
-  if obfuscate:
-    hashedStr = b64c(sha512b64('%s%s' % (s, obfuscate)).lower())
-  else: #Don't obfuscate
-    hashedStr = re.sub('[^0123456789abcdefghijklmnopqrstuvwxyz]+', '',
-                s.lower())[:(length-4)]
-    while len(hashedStr) < length:
-      hashedStr += b64c(sha1b64(s)).lower()
-  return hashedStr[:length]
+    Keyword arguments:
+    s -- The string to be hashed
+    length -- The length of the hash to create.
+                        Might be limited by the hash method
+    obfuscate -- None to disable SHA512 obfuscation,
+                             or a salt to append to the string
+                             before hashing
+    """
+    if obfuscate:
+        hashedStr = b64c(sha512b64('%s%s' % (s, obfuscate)).lower())
+    else:  # Don't obfuscate
+        hashedStr = re.sub('[^0123456789abcdefghijklmnopqrstuvwxyz]+', '',
+                           s.lower())[:(length - 4)]
+        while len(hashedStr) < length:
+            hashedStr += b64c(sha1b64(s)).lower()
+    return hashedStr[:length]
+
 
 def b36(number):
-  """
-  Convert a number to base36
+    """
+    Convert a number to base36
 
-  >>> b36(2701)
-  '231'
-  >>> b36(12345)
-  '9IX'
-  >>> b36(None)
-  '0'
+    >>> b36(2701)
+    '231'
+    >>> b36(12345)
+    '9IX'
+    >>> b36(None)
+    '0'
 
-  Keyword arguments:
-  number -- An integer to convert to base36
-  """
-  alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  base36 = ''
-  while number:
-    number, i = divmod(number, 36)
-    base36 = alphabet[i] + base36
-  return base36 or alphabet[0]
+    Keyword arguments:
+    number -- An integer to convert to base36
+    """
+    alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    base36 = ''
+    while number:
+        number, i = divmod(number, 36)
+        base36 = alphabet[i] + base36
+    return base36 or alphabet[0]
 
 
 GPG_BEGIN_MESSAGE = '-----BEGIN PGP MESSAGE'
 GPG_END_MESSAGE = '-----END PGP MESSAGE'
+
+
 def decrypt_gpg(lines, fd):
-  for line in fd:
-    lines.append(line)
-    if line.startswith(GPG_END_MESSAGE):
-      break
+    for line in fd:
+        lines.append(line)
+        if line.startswith(GPG_END_MESSAGE):
+            break
 
-  gpg = subprocess.Popen(['gpg', '--batch'],
-                         stdin=subprocess.PIPE,
-                         stderr=subprocess.PIPE,
-                         stdout=subprocess.PIPE)
-  lines = gpg.communicate(input=''.join(lines))[0].splitlines(True)
-  if gpg.wait() != 0:
-    raise AccessError("GPG was unable to decrypt the data.")
+    gpg = subprocess.Popen(['gpg', '--batch'],
+                                                 stdin=subprocess.PIPE,
+                                                 stderr=subprocess.PIPE,
+                                                 stdout=subprocess.PIPE)
+    lines = gpg.communicate(input=''.join(lines))[0].splitlines(True)
+    if gpg.wait() != 0:
+        raise AccessError("GPG was unable to decrypt the data.")
 
-  return  lines
+    return lines
+
 
 def decrypt_and_parse_lines(fd, parser):
-  size = 0
-  for line in fd:
-    size += len(line)
-    if line.startswith(GPG_BEGIN_MESSAGE):
-       for line in decrypt_gpg([line], fd):
-         parser(line.decode('utf-8'))
-    else:
-      parser(line.decode('utf-8'))
-  return size
+    size = 0
+    for line in fd:
+        size += len(line)
+        if line.startswith(GPG_BEGIN_MESSAGE):
+            for line in decrypt_gpg([line], fd):
+                parser(line.decode('utf-8'))
+        else:
+            parser(line.decode('utf-8'))
+    return size
+
 
 def gpg_open(filename, recipient, mode):
-  fd = open(filename, mode)
-  if recipient and ('a' in mode or 'w' in mode):
-    gpg = subprocess.Popen(['gpg', '--batch', '-aer', recipient],
-                           stdin=subprocess.PIPE,
-                           stdout=fd)
-    return gpg.stdin
-  return fd
+    fd = open(filename, mode)
+    if recipient and ('a' in mode or 'w' in mode):
+        gpg = subprocess.Popen(['gpg', '--batch', '-aer', recipient],
+                               stdin=subprocess.PIPE,
+                               stdout=fd)
+        return gpg.stdin
+    return fd
 
 
 # Indexing messages is an append-heavy operation, and some files are
@@ -206,110 +225,110 @@ APPEND_FD_CACHE = {}
 APPEND_FD_CACHE_SIZE = 500
 APPEND_FD_CACHE_ORDER = []
 APPEND_FD_CACHE_LOCK = threading.Lock()
+
+
 def flush_append_cache(ratio=1, count=None, lock=True):
-  try:
-    if lock: APPEND_FD_CACHE_LOCK.acquire()
-    drop = count or int(ratio*len(APPEND_FD_CACHE_ORDER))
-    for fn in APPEND_FD_CACHE_ORDER[:drop]:
-      try:
-        APPEND_FD_CACHE[fn].close()
-        del APPEND_FD_CACHE[fn]
-      except KeyError:
-        pass
-    APPEND_FD_CACHE_ORDER[:drop] = []
-  finally:
-    if lock: APPEND_FD_CACHE_LOCK.release()
+    try:
+        if lock:
+            APPEND_FD_CACHE_LOCK.acquire()
+        drop = count or int(ratio * len(APPEND_FD_CACHE_ORDER))
+        for fn in APPEND_FD_CACHE_ORDER[:drop]:
+            try:
+                APPEND_FD_CACHE[fn].close()
+                del APPEND_FD_CACHE[fn]
+            except KeyError:
+                pass
+        APPEND_FD_CACHE_ORDER[:drop] = []
+    finally:
+        if lock:
+            APPEND_FD_CACHE_LOCK.release()
+
 
 def cached_open(filename, mode):
-  try:
-    APPEND_FD_CACHE_LOCK.acquire()
-    if mode == 'a':
-      fd = None
-      if filename in APPEND_FD_CACHE:
-        APPEND_FD_CACHE_ORDER.remove(filename)
-        fd = APPEND_FD_CACHE[filename]
-      if not fd or fd.closed:
-        if len(APPEND_FD_CACHE) > APPEND_FD_CACHE_SIZE:
-          flush_append_cache(count=1, lock=False)
-        try:
-          fd = APPEND_FD_CACHE[filename] = open(filename, 'a')
-        except (IOError, OSError):
-          # Too many open files?  Close a bunch and try again.
-          flush_append_cache(ratio=0.3, lock=False)
-          fd = APPEND_FD_CACHE[filename] = open(filename, 'a')
-      APPEND_FD_CACHE_ORDER.append(filename)
-      return fd
-    else:
-      if filename in APPEND_FD_CACHE:
-        fd = APPEND_FD_CACHE[filename]
-        try:
-          if 'w' in mode or '+' in mode:
-            del APPEND_FD_CACHE[filename]
-            APPEND_FD_CACHE_ORDER.remove(filename)
-            fd.close()
-          else:
-            fd.flush()
-        except (ValueError, IOError):
-          pass
-      return open(filename, mode)
-  finally:
-    APPEND_FD_CACHE_LOCK.release()
+    try:
+        APPEND_FD_CACHE_LOCK.acquire()
+        if mode == 'a':
+            fd = None
+            if filename in APPEND_FD_CACHE:
+                APPEND_FD_CACHE_ORDER.remove(filename)
+                fd = APPEND_FD_CACHE[filename]
+            if not fd or fd.closed:
+                if len(APPEND_FD_CACHE) > APPEND_FD_CACHE_SIZE:
+                    flush_append_cache(count=1, lock=False)
+                try:
+                    fd = APPEND_FD_CACHE[filename] = open(filename, 'a')
+                except (IOError, OSError):
+                    # Too many open files?    Close a bunch and try again.
+                    flush_append_cache(ratio=0.3, lock=False)
+                    fd = APPEND_FD_CACHE[filename] = open(filename, 'a')
+            APPEND_FD_CACHE_ORDER.append(filename)
+            return fd
+        else:
+            if filename in APPEND_FD_CACHE:
+                fd = APPEND_FD_CACHE[filename]
+                try:
+                    if 'w' in mode or '+' in mode:
+                        del APPEND_FD_CACHE[filename]
+                        APPEND_FD_CACHE_ORDER.remove(filename)
+                        fd.close()
+                    else:
+                        fd.flush()
+                except (ValueError, IOError):
+                    pass
+            return open(filename, mode)
+    finally:
+        APPEND_FD_CACHE_LOCK.release()
 
-
-import StringIO
-try:
-  import Image
-except:
-  Image = None
 
 def thumbnail(fileobj, output_fd, height=None, width=None):
-  """
-  Generates a thumbnail image , which should be a file,
-  StringIO, or string, containing a PIL-supported image.
-  FIXME: Failure modes unmanaged.
+    """
+    Generates a thumbnail image , which should be a file,
+    StringIO, or string, containing a PIL-supported image.
+    FIXME: Failure modes unmanaged.
 
-  Keyword arguments:
-  fileobj -- Either a StringIO instance, a file object or
-             a string (containing the image) to
-             read the source image from
-  output_fd -- A file object or filename, or StringIO to
-  """
-  if not Image:
-    # If we don't have PIL, we just return the supplied filename in the hopes
-    # that somebody had the good sense to extract the right attachment to that
-    # filename...
-    return None
+    Keyword arguments:
+    fileobj -- Either a StringIO instance, a file object or
+                         a string (containing the image) to
+                         read the source image from
+    output_fd -- A file object or filename, or StringIO to
+    """
+    if not Image:
+        # If we don't have PIL, we just return the supplied filename in
+        # the hopes that somebody had the good sense to extract the
+        # right attachment to that filename...
+        return None
 
-  #Ensure the source image is either a file-like object or a StringIO
-  if not isinstance(fileobj, StringIO.StringIO) and not isinstance(fileobj, file):
-    fileobj = StringIO.StringIO(fileobj)
+    #Ensure the source image is either a file-like object or a StringIO
+    if (not isinstance(fileobj, StringIO.StringIO) and
+            not isinstance(fileobj, file)):
+        fileobj = StringIO.StringIO(fileobj)
 
-  image = Image.open(fileobj)
+    image = Image.open(fileobj)
 
-  # defining the size
-  if height == None and width == None:
-    raise Exception("Must supply width or height!")
-  # If only one coordinate is given, calculate the
-  # missing one in order to make the thumbnail
-  # have the same proportions as the source img
-  if height and not width:
-    x = height
-    y = int((float(height)/image.size[0]) * image.size[1])
-  elif width and not height:
-    y = width
-    x = int((float(width)/image.size[1]) * image.size[0])
-  else: #We have both sizes
-    y = width
-    x = height
-  image.thumbnail([x, y], Image.ANTIALIAS)
-  # If saving an optimized image fails, save it unoptimized
-  # Keep the format (png, jpg) of the source image
-  try:
-    image.save(output_fd, format=image.format, quality=90, optimize=1)
-  except:
-    image.save(output_fd, format=image.format, quality=90)
+    # defining the size
+    if height is None and width is None:
+        raise Exception("Must supply width or height!")
+    # If only one coordinate is given, calculate the
+    # missing one in order to make the thumbnail
+    # have the same proportions as the source img
+    if height and not width:
+        x = height
+        y = int((float(height) / image.size[0]) * image.size[1])
+    elif width and not height:
+        y = width
+        x = int((float(width) / image.size[1]) * image.size[0])
+    else:  # We have both sizes
+        y = width
+        x = height
+    image.thumbnail([x, y], Image.ANTIALIAS)
+    # If saving an optimized image fails, save it unoptimized
+    # Keep the format (png, jpg) of the source image
+    try:
+        image.save(output_fd, format=image.format, quality=90, optimize=1)
+    except:
+        image.save(output_fd, format=image.format, quality=90)
 
-  return image
+    return image
 
 
 class CleanText:
@@ -326,20 +345,20 @@ class CleanText:
     """
     FS = ':/.\'\"\\'
     NONALNUM = [chr(c) for c in (set(range(32, 127)) -
-                                 set(range(ord('0'), ord('9')+1)) -
-                                 set(range(ord('a'), ord('z')+1)) -
-                                 set(range(ord('A'), ord('Z')+1)))]
+                                 set(range(ord('0'), ord('9') + 1)) -
+                                 set(range(ord('a'), ord('z') + 1)) -
+                                 set(range(ord('A'), ord('Z') + 1)))]
 
     def __init__(self, text, banned=''):
-      self.clean = str("".join([i for i in text if ord(i) > 31 and
-                                                   ord(i) < 127 and
-                                                   i not in banned]))
+        self.clean = str("".join([i for i in text if ord(i) > 31 and
+                                                     ord(i) < 127 and
+                                                     i not in banned]))
 
     def __str__(self):
-      return str(self.clean)
+        return str(self.clean)
 
     def __unicode__(self):
-      return unicode(self.clean)
+        return unicode(self.clean)
 
 
 # If 'python util.py' is executed, start the doctest unittest
