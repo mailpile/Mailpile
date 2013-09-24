08f279afee97ab56786c45dabf8a3ac362d1acd9
Bjarni R. Einarsson <bre@klaki.net>
2013-09-24 19:57:59 +0000

    PEP8 and refactor

--- mailpile/plugins/setup.py
+++ mailpile/plugins/setup.py
@@ -2,30 +2,34 @@ import mailpile.plugins
 from mailpile.commands import Command
 from mailpile.util import *
 
-from mailpile.plugins.tags import Tag, Filter
+from mailpile.plugins.tags import AddTag, Filter
 
 
 ##[ Commands ]################################################################
 
 class Setup(Command):
-  """Perform initial setup"""
-  ORDER = ('Internals', 0)
-  def command(self):
-    session = self.session
+    """Perform initial setup"""
+    SYNOPSIS = (None, 'setup', None, None)
+    ORDER = ('Internals', 0)
 
-    # Create local mailboxes
-    session.config.open_local_mailbox(session)
+    def command(self):
+        session = self.session
 
-    # Create standard tags and filters
-    tags = session.config.get('tag', {}).values()
-    for t in ('New', 'Inbox', 'Spam', 'Drafts', 'Sent', 'Trash'):
-      if t not in tags:
-        Tag(session, arg=['add', t]).run()
-    if 'New' not in tags:
-      Filter(session, arg=['new', '+Inbox', '+New', 'New Mail filter']).run()
-      Filter(session, arg=['read', '-New', 'Read Mail filter']).run()
+        # Create local mailboxes
+        session.config.open_local_mailbox(session)
 
-    return True
+        # Create standard tags and filters
+        tags = session.config.get('tag', {}).values()
+        for t in ('New', 'Inbox', 'Spam', 'Drafts', 'Sent', 'Trash'):
+            if t not in tags:
+                AddTag(session, arg=[t]).run()
+        if 'New' not in tags:
+            Filter(session,
+                   arg=['new', '+Inbox', '+New', 'New Mail filter']).run()
+            Filter(session,
+                   arg=['read', '-New', 'Read Mail filter']).run()
+
+        return True
 
 
-mailpile.plugins.register_command('_setup', 'setup', Setup)
+mailpile.plugins.register_commands(Setup)
