002161f0719b5ea8b15048b994e0477c7022f956
Smári McCarthy <smari@immi.is>
2013-10-08 10:13:05 +0100

    Moving superfluous time/date data from the search results and creating helper functions in Jinja.

--- mailpile/jinjaextensions.py
+++ mailpile/jinjaextensions.py
@@ -2,6 +2,8 @@ from jinja2 import nodes
 from jinja2.ext import Extension
 from jinja2.utils import contextfunction, import_string, Markup
 from commands import Action
+import re
+import datetime
 
 class MailpileCommand(Extension):
     """Run Mailpile Commands, """
@@ -11,6 +13,25 @@ class MailpileCommand(Extension):
         Extension.__init__(self, environment)
         self.env = environment
         environment.globals['mailpile'] = self._command
+        environment.globals['regex_replace'] = self._regex_replace
+        environment.globals['friendly_date'] = self._friendly_date
 
     def _command(self, command, *args, **kwargs):
         return Action(self.env.session, command, args, data=kwargs).as_dict()
+
+    def _regex_replace(self, s, find, replace):
+        return re.sub(find, replace, s)
+
+    def _friendly_date(self, timestamp):
+        days_ago = (datetime.datetime() - timestamp) / (24*3600)
+
+        if days_ago < 1:
+            return 'today'
+        elif days_ago < 2:
+            return 'yesterday'
+        elif days_ago < 7:
+            return '%d days ago' % days_ago
+        else:
+            return timestamp.strftime("%Y-%m-%d")
+
+
