1e57851f80f0ce10fac124cad92cf76c24e9e434
Brennan Novak <hi@brennannovak.com>
2013-10-08 10:09:33 +0100

    Added jinja extension for regex_replace

--- mailpile/jinjaextensions.py
+++ mailpile/jinjaextensions.py
@@ -2,6 +2,7 @@ from jinja2 import nodes
 from jinja2.ext import Extension
 from jinja2.utils import contextfunction, import_string, Markup
 from commands import Action
+import re
 
 class MailpileCommand(Extension):
     """Run Mailpile Commands, """
@@ -11,6 +12,11 @@ class MailpileCommand(Extension):
         Extension.__init__(self, environment)
         self.env = environment
         environment.globals['mailpile'] = self._command
+        environment.globals['regex_replace'] = self._regex_replace
 
     def _command(self, command, *args, **kwargs):
         return Action(self.env.session, command, args, data=kwargs).as_dict()
+
+    def _regex_replace(self, s, find, replace):
+        """A non-optimal implementation of a regex filter"""
+        return re.sub(find, replace, s)
