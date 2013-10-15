789aa388d9dbf239d3a4a8368367743f37435357
Brennan Novak <hi@brennannovak.com>
2013-10-15 20:20:05 +0000

    Added some basic drag & drop AJAX calls to tag API. Issue #213

--- static/default/js/mailpile-min.js
+++ static/default/js/mailpile-min.js
@@ -366,33 +366,6 @@ $(document).on('click', 'a.change-view-size', function(e) {
 
 
 /* **********************************************
-     Begin search.js
-********************************************** */
-
-/* Search */
-
-
-	/* Hide Various Things */
-	$('#search-params, #bulk-actions').hide();
-
-	/* Search Box */
-	$('#qbox').bind("focus", function(key) {
-		$('#search-params').slideDown('fast');
-	});
-
-	$('#qbox').bind("blur", function(key) {
-		$('#search-params').slideUp('fast');
-	});
-
-	for (item in keybindings) {
-		if (item[1] == "global") {
-			Mousetrap.bindGlobal(item[0], item[2]);
-		} else {
-			Mousetrap.bind(item[0], item[2]);
-		}
-	}
-
-/* **********************************************
      Begin messages.js
 ********************************************** */
 
