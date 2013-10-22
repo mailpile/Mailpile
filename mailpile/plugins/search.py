41027368c0e23c6f342d186538cac909f51392d6
Bjarni R. Einarsson <bre@klaki.net>
2013-10-22 15:07:56 +0000

    Search term encoding fixes

--- mailpile/plugins/search.py
+++ mailpile/plugins/search.py
@@ -113,7 +113,8 @@ class SearchResults(dict):
     self.expand = expand
     self.idx = idx
 
-    self['search_terms'] = terms = session.searched
+    self['search_terms'] = terms = [t.decode('utf-8')
+                                    for t in session.searched]
     if 'tags' in idx.config:
       self['search_tags'] = [idx.config.get_tag(t.split(':')[1], {})
                              for t in terms if t.startswith('in:')]
