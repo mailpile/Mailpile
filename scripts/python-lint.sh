17cff922f3a2c67122ea9a1bc8999217df6316e8
Jon Bringhurst <jon@bringhurst.org>
2013-10-29 18:36:02 +0000

    Narrow linting to the mailpile directory. This is to avoid linting the build or debian directories.

--- scripts/python-lint.sh
+++ scripts/python-lint.sh
@@ -3,7 +3,7 @@ command -v pep8 >/dev/null 2>&1 || {
     echo >&2 "pep8 not found.";
     exit 1;
 }
-pep8 --ignore W191 .
+pep8 --ignore W191 ./mailpile
 if [ $? -eq 0 ]; then
    echo "pep8 returned with success"
    exit 0
