9cf529b033ccb3c0d3e4922c0fcaa094b5961b71
Uli Köhler <ulikoehler@online.de>
2013-11-16 14:55:08 +0100

    Check tag object for being None and fallback else

--- mailpile/urlmap.py
+++ mailpile/urlmap.py
@@ -220,8 +220,8 @@ class UrlMap:
 
         tag_slug = '/'.join([p for p in path_parts[1:] if p])
         tag = self.session.config.get_tag(tag_slug)
-        tag_search = [tag.search_terms % tag]
-        if tag.search_order and 'order' not in query_data:
+        tag_search = [tag.search_terms % tag] if tag is not None else [""]
+        if tag is not None and tag.search_order and 'order' not in query_data:
             query_data['order'] = [tag.search_order]
 
         if pos:
