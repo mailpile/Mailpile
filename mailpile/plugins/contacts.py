215910e8fe261ddf1c8ced0b4bc5df988f3a0b1e
Smári McCarthy <smari@immi.is>
2013-11-27 12:38:19 +0000

    Added last message information to contact view.

--- mailpile/plugins/contacts.py
+++ mailpile/plugins/contacts.py
@@ -222,6 +222,43 @@ class Contact(ContactVCard(VCard)):
     """View contacts"""
     SYNOPSIS = (None, 'contact', 'contact', '[<email>]')
 
+    def command(self, save=True):
+        contact = VCard.command(self, save)
+        # Tee-hee, monkeypatching results.
+        contact["sent_messages"] = 0
+        contact["received_messages"] = 0
+        contact["last_contact_from"] = 10000000000000
+        contact["last_contact_to"] = 10000000000000
+
+        try:
+            for email in contact["contact"]["email"]:
+                s = Action(self.session, "search", ["in:Sent", "to:%s" % (email["email"])]).as_dict()
+                print "TO: ", s
+                contact["sent_messages"] += s["result"]["total"]
+                for msg in s["result"]["messages"]:
+                    if msg["timestamp"] < contact["last_contact_to"]:
+                        contact["last_contact_to"] = msg["timestamp"]
+                        contact["last_contact_to_msg_url"] = msg["url"]
+
+                s = Action(self.session, "search", ["from:%s" % (email["email"])]).as_dict()
+                print "FROM: ", s
+                contact["received_messages"] += s["result"]["total"]
+                for msg in s["result"]["messages"]:
+                    if msg["timestamp"] < contact["last_contact_from"]:
+                        contact["last_contact_from"] = msg["timestamp"]
+                        contact["last_contact_from_msg_url"] = msg["url"]
+        except Exception, e:
+            print "ERROR:", e
+
+        if contact["last_contact_to"] == 10000000000000:
+            contact["last_contact_to"] = False
+            contact["last_contact_to_msg_url"] = ""
+
+        if contact["last_contact_from"] == 10000000000000:
+            contact["last_contact_from"] = False
+            contact["last_contact_from_msg_url"] = ""
+
+        return contact
 
 class AddContact(ContactVCard(AddVCard)):
     """Add contacts"""
