36a67e3bad616863d81c4cce31eeefc7b12ba813
Smári McCarthy <smari@immi.is>
2013-12-17 15:39:19 +0000

    Merging PGPMime into GPG interface.

--- mailpile/gpgi.py
+++ mailpile/gpgi.py
@@ -6,6 +6,9 @@ import time
 import re
 import tempfile
 from subprocess import Popen, PIPE
+from email.parser import Parser
+from email.message import Message
+import StringIO
 
 def _(x): return x
 
@@ -584,6 +587,80 @@ u:Smari McCarthy <smari@immi.is>::scESC:\\nsub:u:4096:1:13E0BB42176BA0AC:\
                 res[key] = props
 
         return res
+
+
+
+class PGPMimeParser(Parser):
+
+    def parse_pgpmime(self, message):
+        sig_count, sig_parts, sig_alg = 0, [], 'SHA1'
+        enc_count, enc_parts, enc_ver = 0, [], None
+
+        for part in message.walk():
+            mimetype = part.get_content_type()
+            if (sig_count > 1) and (mimetype == 'application/pgp-signature'):
+                sig = part.get_payload()
+                msg = '\r\n'.join(sig_parts[0].as_string().splitlines(False))+'\r\n'
+
+                result = None
+                gpg = GnuPG()
+                status = gpg.verify(msg, sig)
+                print status
+
+                for sig_part in sig_parts:
+                    print "sig_part: ", dir(sig_part), type(sig_part)
+                    sig_part.openpgp = ("signed", status)
+                    part.openpgp = ("signed", status)
+
+                # Reset!
+                sig_count, sig_parts = 0, []
+
+            elif sig_count > 0:
+                sig_parts.append(part)
+                sig_count += 1
+
+            elif enc_count > 0 and (mimetype == 'application/octet-stream'):
+                # FIXME: Decrypt and parse!
+                crypt = tempfile.NamedTemporaryFile()
+                crypt.write(part.get_payload())
+                crypt.flush()
+                msg = '\r\n'.join(part.as_string().splitlines(False))+'\r\n'
+
+                result = None
+                gpg = GnuPG()
+                encryption_info, text = gpg.decrypt(msg)
+                if encryption_info["status"] == "decrypted":
+                    summary = ('decrypted', text)
+                else:
+                    summary = ('encrypted', text)
+                s = StringIO.StringIO()
+                s.write(result)
+                m = Parser().parse(s)
+                m = Message()
+                m.set_payload(text)
+                part.set_payload([m])
+                part.openpgp = summary
+
+                for enc_part in enc_parts:
+                    enc_part.openpgp = summary
+
+                # Reset!
+                enc_count, enc_parts = 0, []
+
+            elif mimetype == 'multipart/signed':
+                sig_alg = part.get_param('micalg', 'pgp-sha1'
+                                         ).split('-')[-1].upper()
+                sig_count = 1
+
+            elif mimetype == 'multipart/encrypted':
+                enc_count = 1
+
+    def parse(self, fp, headersonly=False):
+        message = Parser.parse(self, fp, headersonly=headersonly)
+        self.parse_pgpmime(message)
+        return message
+
+
 
 
 
