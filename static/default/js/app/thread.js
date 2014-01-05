007b10e2b748fd4ac377817794e795ebb4859938
Brennan Novak <hi@brennannovak.com>
2014-01-05 20:13:00 +0000

    Added JS files from static thread view

--- static/default/js/app/thread.js
+++ static/default/js/app/thread.js
@@ -6,3 +6,49 @@ MailPile.prototype.view = function(idx, msgid) {
 		$("#results").empty();
 	});
 };
+
+/* Thread - Show People In Conversation */
+$(document).on('click', '.thread-show-people', function() {
+
+  alert('FIXME: Show all people in conversation');
+});
+
+/* Thread - Show Security */
+$(document).on('click', '.thread-show-security', function() {
+  
+  alert('FIXME: Show details about security of thread');
+})
+
+/* Thread - Expand Snippet */
+$(document).on('click', '.thread-snippet', function() {
+  
+  alert('FIXME: Will load full message with mid: ' + $(this).data('mid'));
+});
+
+
+/* Thread - Might Move to Global Location / Abstraction */
+$(document).on('click', '.dropdown-toggle', function() {
+  console.log('toggle da bizzle');
+  $(this).find('.icon-arrow-right').removeClass('icon-arrow-right').addClass('icon-arrow-down');
+});
+
+
+/* Compose - Pick Send Date */
+$(document).on('click', '.pick-send-datetime', function(e) {
+
+  if ($(this).data('datetime') == 'immediately') {
+    $('#reply-datetime-display').html($(this).html());
+  }
+  else {
+    $('#reply-datetime-display').html('in ' + $(this).html());
+  }
+
+  $('#reply-datetime span.icon').removeClass('icon-arrow-down').addClass('icon-arrow-right');
+
+});
+
+
+/* Compose - Details */
+$(document).on('click', '#reply-show-details', function(e) {
+  $('#thread-reply-details').slideDown('fast');
+});
