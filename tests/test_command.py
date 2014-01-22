42017a11d85e76c36fd9b6ba7026c0ea2cc56ac6
Bjarni R. Einarsson <bre@klaki.net>
2014-01-22 16:34:42 +0000

    Fix test broken by title work

--- tests/test_command.py
+++ tests/test_command.py
@@ -7,7 +7,7 @@ from tests import MailPileUnittest
 class TestCommands(MailPileUnittest):
     def test_index(self):
         res = self.mp.rescan()
-        self.assertEqual(res.as_dict()["message"], 'OK')
+        self.assertEqual(res.as_dict()["message"], 'rescan')
 
     def test_search(self):
         # A random search must return results in less than 0.2 seconds.
