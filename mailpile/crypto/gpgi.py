7fee389b05b4fde8def7fad649ae9dd566b04bfa
Abhilash Raj <raj.abhilash1@gmail.com>
2014-03-18 22:35:50 +0530

    export import_key to rest API

--- mailpile/crypto/gpgi.py
+++ mailpile/crypto/gpgi.py
@@ -542,20 +542,15 @@ u:Smari McCarthy <smari@immi.is>::scESC:\\nsub:u:4096:1:13E0BB42176BA0AC:\
         else:
             return []
 
-    def import_keys(self, key_data=None, key_file=None):
+    def import_keys(self, key_data=None):
         """
-        Imports gpg keys from a file object or string.
+        Imports gpg keys from a string.
         """
-        if key_data and not key_file:
-            key_file = StringIO(key_data)
-
-        retvals = self.run(["--import", output=key_file.read()])
-        key_file.close()
-        print retvals[1]["status"]
+        retvals = self.run(["--import"], output=key_data)
+        res = {"imported": [], "updated": [], "failed": []}
         for x in retvals[1]["status"]:
             if x[0] == "IMPORTED":
                 res["imported"].append({
-                    "keyid": keyid,
                     "fingerprint": x[1],
                     "username": x[2]
                 })
@@ -569,7 +564,6 @@ u:Smari McCarthy <smari@immi.is>::scESC:\\nsub:u:4096:1:13E0BB42176BA0AC:\
                     "16": "contains private key",
                 }
                 res["updated"].append({
-                    "keyid": keyid,
                     "details": int(x[1]),
                     "details_text": reasons[x[1]],
                     "fingerprint": x[2],
@@ -583,7 +577,6 @@ u:Smari McCarthy <smari@immi.is>::scESC:\\nsub:u:4096:1:13E0BB42176BA0AC:\
                     "4": "error storing certificate",
                 }
                 res["failed"].append({
-                    "keyid": keyid,
                     "details": int(x[1]),
                     "details_text": reasons[x[1]],
                     "fingerprint": x[2]
@@ -605,6 +598,7 @@ u:Smari McCarthy <smari@immi.is>::scESC:\\nsub:u:4096:1:13E0BB42176BA0AC:\
                     "skipped_new_keys": int(x[13]),
                     "not_imported": int(x[14]),
                 }
+        return res
 
 
     class ResultParser:
