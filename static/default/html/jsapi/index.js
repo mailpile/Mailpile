39d43b36c640210fa93f9f46ceeadcdc9e5ce724
Smári McCarthy <smari@mailpile.is>
2014-04-15 19:39:37 +0100

    Undid previous blooper.

--- static/default/html/jsapi/index.js
+++ static/default/html/jsapi/index.js
@@ -84,11 +84,9 @@ new_mailpile.api = (function() {
 
    FIXME: Make sure the order is somehow sane given dependenies.
 */
-$(document).ready(function() {
-    {% for js_class in result.javascript_classes %}
-    new_{{ js_class.classname }} = {% if js_class.code %}(function(){
-    {{ js_class.code|safe }}})(); /* EOF:{{ js_class.classname }} */
-    {% else %}{};
-    {% endif %}
-    {% endfor %}
-});
+{% for js_class in result.javascript_classes %}
+new_{{ js_class.classname }} = {% if js_class.code %}(function(){
+{{ js_class.code|safe }}})(); /* EOF:{{ js_class.classname }} */
+{% else %}{};
+{% endif %}
+{% endfor %}
