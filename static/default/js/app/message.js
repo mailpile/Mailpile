8193f741494495cd9b7e11c6a60fbdd01999664a
Brennan Novak <hi@brennannovak.com>
2014-04-23 17:19:25 +0000

    Changed redirect URL on message 'Unthread' behavior

--- static/default/js/app/message.js
+++ static/default/js/app/message.js
@@ -31,8 +31,19 @@ $(document).on('click', '.message-action-spam', function() {
 });
 
 $(document).on('click', '.message-action-unthread', function() {
-  var mid = $(this).parent().parent().data('mid');
-  console.log('This will Unthread a single message'); 
+  var mid = $(this).parent().parent().parent().parent().data('mid');
+  $.ajax({
+    url      : '/api/0/message/unthread/',
+    type     : 'POST',
+    data     : { mid: mid },
+    success  : function(response) {
+      if (response.status === 'success') {
+        window.location.href = mailpile.urls.message_sent + mid + '/';
+      } else {
+        mailpile.notification(response.status, response.message);
+      }
+    }
+  });
 });
 
 $(document).on('click', '.message-action-trash', function() {
