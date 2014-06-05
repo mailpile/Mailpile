d4de936a81613ae75bb68151ad11e3be3d5440d7
Brennan Novak <hi@brennannovak.com>
2014-06-05 13:47:28 +0200

    Fixed resize JS that was messed up from merge issues

--- static/default/js/mailpile.js
+++ static/default/js/mailpile.js
@@ -117,7 +117,11 @@ MailPile.prototype.render = function() {
     var content_tools_height    = $('#content-tools').height();
     var new_content_width       = $(window).width() - sidebar_width;
     var new_content_view_height = content_height - content_tools_height;
-  
+
+    $('#content-tools').css('position', 'fixed');
+    $('.sub-navigation').width(content_width);
+    $('#thread-title').width(content_width);
+
     // Set Content View
     $('#content, #content-wide').css({'height': content_height});
     $('#content-tools, .sub-navigation, .bulk-actions').width(new_content_width);
