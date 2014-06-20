4ad484a56e865dfecf86dbbc88e15a96fef55c7a
Smári McCarthy <smari@mailpile.is>
2014-06-20 17:49:21 +0200

    Adding keybindings for pile navigation

--- static/default/js/app/bulk_actions.js
+++ static/default/js/app/bulk_actions.js
@@ -32,6 +32,82 @@ MailPile.prototype.bulk_action_unread = function() {
 };
 
 
+MailPile.prototype.bulk_action_select_all = function() {
+  var checkboxes = $('#pile-results input[type=checkbox]');
+  $.each(checkboxes, function() {      
+    mailpile.pile_action_select($(this).parent().parent());
+  });
+  $("#pile-select-all-action").attr('checked','checked');
+}
+
+MailPile.prototype.bulk_action_select_none = function() {
+  var checkboxes = $('#pile-results input[type=checkbox]');
+  $.each(checkboxes, function() {
+    mailpile.pile_action_unselect($(this).parent().parent());
+  });
+  $("#pile-select-all-action").removeAttr('checked');
+}
+
+MailPile.prototype.bulk_action_select_invert = function() {
+  var checkboxes = $('#pile-results input[type=checkbox]');
+  $.each(checkboxes, function() {
+    if ($(this).is(":checked")) {
+      mailpile.pile_action_unselect($(this).parent().parent());
+    } else {
+      mailpile.pile_action_select($(this).parent().parent());
+    }
+  });
+  if (this['messages_cache'].length == checkboxes.length) {
+    $("#pile-select-all-action").attr('checked','checked');
+  } else if (this['messages_cache'].length == 0) {
+    $("#pile-select-all-action").removeAttr('checked');
+  }
+}
+
+
+MailPile.prototype.bulk_action_selection_up = function() {
+  var checkboxes = $('#pile-results input[type=checkbox]');
+  if (this['messages_cache'].length == 0) {
+    mailpile.pile_action_select($(checkboxes[checkboxes.length-1]).parent().parent());
+    return;
+  }
+  $.each(checkboxes, function() {
+    if ($(this).parent().parent().next().children().children("input").is(":checked")) {
+      mailpile.pile_action_select($(this).parent().parent());
+    } else {
+      mailpile.pile_action_unselect($(this).parent().parent());
+    }
+  });
+}
+
+MailPile.prototype.bulk_action_selection_down = function() {
+  var checkboxes = $('#pile-results input[type=checkbox]');
+  if (this['messages_cache'].length == 0) {
+    mailpile.pile_action_select($(checkboxes[0]).parent().parent());
+    return;
+  }
+
+  $(checkboxes.get().reverse()).each(function() {
+    if ($(this).parent().parent().prev().children().children("input").is(":checked")) {
+      mailpile.pile_action_select($(this).parent().parent());
+    } else {
+      mailpile.pile_action_unselect($(this).parent().parent());
+    }
+  });
+}
+
+
+MailPile.prototype.open_selected_thread = function() {
+  if (this['messages_cache'].length == 1) {
+    $("#pile-results input[type=checkbox]:checked").each(function() {
+      window.location.href = $(this).parent().parent()
+                                    .children(".subject")
+                                    .children("a").attr("href");
+    });
+  }
+}
+
+
 /* Bulk Action - Tag */
 $(document).on('click', '.bulk-action-tag', function() {
   mailpile.render_modal_tags();
