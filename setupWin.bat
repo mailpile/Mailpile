6944be2e0664d945c8863e7c0898fe15b2d58abc
sisso19 <sigurthor12@ru.is>
2014-06-23 20:49:41 +0000

    Finish message added to batch file

--- setupWin.bat
+++ setupWin.bat
@@ -5,6 +5,7 @@ if exist c:\python27\python.exe (
 ) else (
   set PYTHONBIN=python
 )
-echo Please Wait While We Get The Requirements
+echo Please Wait While We Get And Install The Requirements
 c:\python27\python.exe -m pip install -r requirements.txt 
+echo Install Is Finished
 PAUSE%*
