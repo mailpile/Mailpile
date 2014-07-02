87d0b165bc70344de9261c60a284c274a0451334
Brennan Novak <hi@brennannovak.com>
2014-07-02 10:03:07 +0100

    Attempted to disable Heartbeat warning again

--- static/default/html/jsapi/app/notifications.js
+++ static/default/html/jsapi/app/notifications.js
@@ -57,7 +57,7 @@ EventLog.play = function() {
 
 EventLog.heartbeat_warning = function() {
     console.log('heartbeat_warning() just fired');
-    EventLog.cancelwarning = Mailpile.notification("warning", "Having trouble connecting to Mailpile... will retry in a few seconds.");
+    // DISABLED: EventLog.cancelwarning = Mailpile.notification("warning", "Having trouble connecting to Mailpile... will retry in a few seconds.");
     EventLog.poll();
 }
 
