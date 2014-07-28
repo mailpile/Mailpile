22dc45b64c2c8c1929ed6f9076ef910a81b3edcb
Brennan Novak <hi@brennannovak.com>
2014-07-28 12:39:11 +0000

    Added JS .serializeObject() helper for Backbone model updating

--- static/default/js/libraries/helpers.js
+++ static/default/js/libraries/helpers.js
@@ -55,3 +55,21 @@ function abbrNum(number, decPlaces) {
 
     return number;
 }
+
+
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
