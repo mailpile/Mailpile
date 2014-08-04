c1b1d63b78b44eb7b77fe43411517b4f4804495d
Brennan Novak <hi@brennannovak.com>
2014-08-04 00:43:23 +0000

    Got Profiling adding and such pretty much locked down

--- static/default/html/jsapi/setup/profiles.js
+++ static/default/html/jsapi/setup/profiles.js
@@ -1,6 +1,14 @@
 /* Setup - Profiles - Model */
-var ProfilesModel = Backbone.Model.extend({
+var ProfileModel = Backbone.Model.extend({
   url: '/setup/profiles/',
+  defaults: {
+    id: 'new',
+    action: 'Add',
+    name: '',
+    email: '',
+    pass: '',
+    note: ''
+  },
   validation: {
     name: {
       minLength: 1,
@@ -8,20 +16,28 @@ var ProfilesModel = Backbone.Model.extend({
       required: true,
       msg: 'Enter a name for your profile'
     },
-    note: {
-      maxLength: 48,
-      required: false
-    },
     email: {
       maxLength: 128,
       pattern: 'email',
       required: true,
       msg: 'Enter a valid email address'
     },
-    password: {
+    pass: {
       required: false
-    }
+    },
+    note: {
+      maxLength: 48,
+      required: false
+    },
+    auto_configurable: false,
+    pgp_keys: []
   }
+});
+
+
+var ProfilesCollection = Backbone.Collection.extend({
+  url: '/setup/profiles/as.json',
+  model: ProfileModel
 });
 
 
@@ -35,36 +51,63 @@ var ProfilesView = Backbone.View.extend({
     return this;
   },
   events: {
-    "click #btn-setup-show-add-profile"   : "showAddProfile",
-    "click #btn-setup-add-profile"        : "processAddProfile",
-    "click .setup-profile-remove"         : "processRemoveProfile"
+    "click #btn-setup-show-add-profile"   : "showAdd",
+    "click #btn-setup-profile-save"       : "processSettings",
+    "click .setup-profile-remove"         : "processRemove"
   },
   show: function() {
+
     this.$el.html($('#template-setup-profiles').html());
+
+    // Load Data & Add to Collection
+    Mailpile.API.setup_profiles_get({}, function(result) {
+
+      if (_.isEmpty(result.result.profiles)) {
+        Backbone.history.navigate('#profiles/add', true);
+      }
+
+      _.each(result.result.profiles, function(val, key) {
+        var profile = new ProfileModel(_.extend({id: key, action: 'Edit'}, val));
+        ProfilesCollection.add(profile);
+        $('#setup-profiles-list-items').append(_.template($('#template-setup-profiles-item').html(), profile.attributes));
+      });
+
+    });
+
+    return this;
   },
-  showAddProfile: function() {
+  showAdd: function() {
     $('#setup-profiles-list').removeClass('bounceInUp').addClass('bounceOutLeft');
-    this.$el.html(_.template($('#template-setup-profiles-add').html()));
+    this.$el.html(_.template($('#template-setup-profiles-add').html(), this.model.attributes));
   },
-  processAddProfile: function(e) {
+  showEdit: function(id) {
+    $('#setup-profiles-list').removeClass('bounceInUp').addClass('bounceOutLeft');
+    var this_profile = ProfilesCollection.get(id).attributes;
+    this.$el.html(_.template($('#template-setup-profiles-add').html(), this_profile));
+  },
+  processSettings: function(e) {
 
     e.preventDefault();
+    if ($(e.target).data('id') == 'new') {
 
-    // Update Model
-    var profile_data = $('#form-setup-profile-add').serializeObject();
-    this.model.set(profile_data);
-
-    // Validate & Process
-    if (!this.model.validate()) {
-      Mailpile.API.setup_profiles_post(profile_data, function(result) {
-        console.log(result);
-      });
+      // Update Model
+      var profile_data = $('#form-setup-profile-settings').serializeObject();
+      this.model.set(profile_data);
+  
+      // Validate & Process
+      if (!this.model.validate()) {
+        Mailpile.API.setup_profiles_post(profile_data, function(result) {
+          Backbone.history.navigate('#profiles', true);
+        });
+      }
+    } else {
+      alert('We do not support editing profiles yet, sorry!');
     }
   },
-  processRemoveProfile: function(e) {
+  processRemove: function(e) {
 
     e.preventDefault();
-    var profile_id = $(e.target).data('profile_id');
+    var profile_id = $(e.target).data('id');
 
     Mailpile.API.profiles_remove_post({ rid: profile_id }, function(result) {
       $('#setup-profile-' + profile_id).fadeOut();
