706d0bc0792f635688e2f9cb82e18ecebea122ff
Bjarni R. Einarsson <bre@klaki.net>
2014-08-06 19:21:11 +0000

    Record PWD to file, to avoid Terminal incompatibilities

--- packages/macosx/mp-in-terminal.sh
+++ packages/macosx/mp-in-terminal.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# This is necessary, since some version/configurations of Terminal will
+# change to a different CWD.  We record where we are now, in a place we
+# are sure to be able to find it later.
+mkdir -p ~/.mailpile
+chmod 700 ~/.mailpile
+pwd > ~/.mailpile/osx.pwd
+
 exec /usr/bin/open \
      -a /Applications/Utilities/Terminal.app \
      Mailpile/packages/macosx/mailpile-osx.sh
