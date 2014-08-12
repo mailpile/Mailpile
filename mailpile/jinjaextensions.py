c1b89866c33c5d2934cb175d1b2efec38995ba90
Brennan Novak <hi@brennannovak.com>
2014-08-12 15:04:57 +0000

    Made nice_fingerprint nice output whole fingerprint

--- mailpile/jinjaextensions.py
+++ mailpile/jinjaextensions.py
@@ -741,7 +741,9 @@ class MailpileCommand(Extension):
     def _nice_fingerprint(self, fingerprint):
         if fingerprint:
             slices = [fingerprint[i:i + 4] for i in range(0, len(fingerprint), 4)]
-            return slices[0] + " " + slices[1] + " " + slices[2] + " " + slices[3]
+            output = slices[0] + " " + slices[1] + " " + slices[2] + " " + slices[3] + " " + slices[4] + " "
+            output +=  slices[5] + " " + slices[6] + " " + slices[7] + " " + slices[8] + " " + slices[9]
+            return output
         else:
             return _("No Fingerprint")
 
