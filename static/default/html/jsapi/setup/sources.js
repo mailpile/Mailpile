5817b4623f8eab76bd3635645293e0556d8d2910
Brennan Novak <hi@brennannovak.com>
2014-08-15 11:37:41 +0000

    Added IF around Source eventlog for local sources without data.connection

--- static/default/html/jsapi/setup/sources.js
+++ static/default/html/jsapi/setup/sources.js
@@ -183,29 +183,33 @@ var SourcesView = Backbone.View.extend({
     // Default Message
     var message = event.message;
 
-    // Connection / Behavior (message)
-    if (event.data.connection.live && !event.data.connection.error[0]) {
+    // Is IMAP or POP
+    if (event.data.connection) {
 
-      // Various Found or Is downloading
-      if (event.data.copying && event.data.copying.running && event.data.copying.total) {
-        message = '{{_("Downloading")}} ' + event.data.copying.copied_messages + ' {{_("of")}} ' + event.data.copying.total + ' {{_("messages")}}';
-      } else if (event.data.copying && event.data.copying.running) {
-        message = '{{_("Found some messages to download")}}';
-      } else if (event.data.rescan) {
-        message = '{{_("Rescanning mailboxes")}}';
+      // Connection / Behavior (message)
+      if (event.data.connection && event.data.connection.live && !event.data.connection.error[0]) {
+  
+        // Various Found or Is downloading
+        if (event.data.copying && event.data.copying.running && event.data.copying.total) {
+          message = '{{_("Downloading")}} ' + event.data.copying.copied_messages + ' {{_("of")}} ' + event.data.copying.total + ' {{_("messages")}}';
+        } else if (event.data.copying && event.data.copying.running) {
+          message = '{{_("Found some messages to download")}}';
+        } else if (event.data.rescan) {
+          message = '{{_("Rescanning mailboxes")}}';
+        }
       }
-    }
-    else if (!event.data.connection.live && !event.data.connection.error[0]) {
-      message = '{{_("Not connected to server")}}';
-    }
-    else if (!event.data.connection.live && event.data.connection.error[0] == 'auth') {
-      message =  '{{_("Can not connect to mailserver")}}';
-      $('#setup-source-notice-' + event.data.id)
-        .html('{{_("The username & password are incorrect")}} <a href="/setup/#sources/' + event.data.id + '">{{_("edit them now")}}</a>')
-        .fadeIn();
-    }
-    else if (!event.data.connection.live && event.data.connection.error[0]) {
-      message = event.data.connection.error[1];
+      else if (!event.data.connection.live && !event.data.connection.error[0]) {
+        message = '{{_("Not connected to server")}}';
+      }
+      else if (!event.data.connection.live && event.data.connection.error[0] == 'auth') {
+        message =  '{{_("Can not connect to mailserver")}}';
+        $('#setup-source-notice-' + event.data.id)
+          .html('{{_("The username & password are incorrect")}} <a href="/setup/#sources/' + event.data.id + '">{{_("edit them now")}}</a>')
+          .fadeIn();
+      }
+      else if (!event.data.connection.live && event.data.connection.error[0]) {
+        message = event.data.connection.error[1];
+      }
     }
 
 
