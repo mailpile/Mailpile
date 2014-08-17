bac98d9220b909fc6a763f4e06abb3fdb14fa593
Brennan Novak <hi@brennannovak.com>
2014-08-17 23:30:53 +0000

    Added first easter egg #827

--- static/default/html/jsapi/setup/security.js
+++ static/default/html/jsapi/setup/security.js
@@ -1,6 +1,72 @@
 /* Setup - Security - Model */
 var SecurityModel = Backbone.Model.extend({
-  validation: {}
+  defaults: {
+    tinfoil: {
+      'encrypt_events': true,
+      'encrypt_index': true,
+      'encrypt_mail': true,
+      'encrypt_misc': true,
+      'encrypt_vcards': true,
+      'index_encrypted': true,
+      'obfuscate_index': true,
+      'use_tor': true,
+      'upload_keyservers': false,
+      'email_key': false,
+      'use_gravatar': false
+    },
+    paranoid: {
+      'encrypt_events': true,
+      'encrypt_index': true,
+      'encrypt_mail': true,
+      'encrypt_misc': true,
+      'encrypt_vcards': true,
+      'index_encrypted': true,
+      'obfuscate_index': true,
+      'use_tor': true,
+      'upload_keyservers': false,
+      'email_key': true,
+      'use_gravatar': false
+    },
+    above: {
+      'encrypt_events': false,
+      'encrypt_index': true,
+      'encrypt_mail': true,
+      'encrypt_misc': true,
+      'encrypt_vcards': true,
+      'index_encrypted': true,
+      'obfuscate_index': true,
+      'use_tor': true,
+      'upload_keyservers': false,
+      'email_key': true,
+      'use_gravatar': true
+    },
+    concerned: {
+      'encrypt_events': false,
+      'encrypt_index': true,
+      'encrypt_mail': false,
+      'encrypt_misc': true,
+      'encrypt_vcards': true,
+      'index_encrypted': true,
+      'obfuscate_index': false,
+      'use_tor': true,
+      'upload_keyservers': true,
+      'email_key': true,
+      'use_gravatar': true
+    },
+    normal: {
+      'encrypt_events': false,
+      'encrypt_index': false,
+      'encrypt_mail': false,
+      'encrypt_misc': false,
+      'encrypt_vcards': false,
+      'index_encrypted': false,
+      'obfuscate_index': false,
+      'use_tor': false,
+      'upload_keyservers': true,
+      'email_key': true,
+      'use_gravatar': true
+    }
+  }
 });
 
 
@@ -9,13 +75,43 @@ var SecurityView = Backbone.View.extend({
   initialize: function() {
 		this.render();
   },
-  render: function(){
+  render: function() {
     return this;
   },
   events: {
-    "click #btn-setup-advanced-access": "showAccess",
+    "click #btn-setup-advanced-access"   : "showAccess",
+    "change #input-setup-security-level" : "actionSecurityLevel",
+    "click #btn-setup-security-save"     : "processSecurity",
   },
   show: function() {
     this.$el.html(_.template($("#template-setup-security").html()));
+  },
+  actionSecurityLevel: function(e) {
+    e.preventDefault();
+    var level = $(e.target).val();
+
+    // Show Some Snark
+    if (level === 'crazy') {
+      $('#modal-full').html($('#modal-security-level-idiot').html());
+      $('#modal-full').modal({ backdrop: true, keyboard: true, show: true, remote: false });
+      $(e.target).val('');
+    }
+    else if (_.indexOf(['tinfoil', 'paranoid', 'above', 'concerned', 'normal'], level) > -1) {
+      _.each(SecurityModel.attributes[level], function(state, name) {
+        $('#form-setup-security').find('input[name='+name+']').prop('checked', state).val(state);
+      });
+    }
+  },
+  processSecurity: function() {
+
+    // Update Model
+    var security_data = $('#form-setup-security').serializeObject();
+
+    // Hide Form
+    Mailpile.API.setup_crypto_post(security_data, function(result) {
+      
+      
+  
+    });
   }
 });
