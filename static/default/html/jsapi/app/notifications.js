7a710d69af69f7f92246acda3475de4028f75ca7
Brennan Novak <hi@brennannovak.com>
2014-08-21 19:51:07 +0000

    First pass at new style JS Notification bubbles

--- static/default/html/jsapi/app/notifications.js
+++ static/default/html/jsapi/app/notifications.js
@@ -1,35 +1,56 @@
 /* Notifications - UI notification at top of window */
-Mailpile.notification = function(status, message_text, complete, complete_action) {
-    var default_messages = {
-        "success" : "Success, we did exactly what you asked.",
-        "info"    : "Here is a basic info update",
-        "debug"   : "What kind of bug is this bug, it's a debug",
-        "warning" : "This here be a warnin to you, just a warnin mind you",
-        "error"   : "Whoa cowboy, you've mozied on over to an error"
-    }
+Mailpile.notification = function(result, complete, complete_action) {
 
-    var message = $('#messages').find('div.' + status);
+  if (result.event_id !== undefined) {
+    result.event_id = result.event_id.split('.').join('-');
+  } else {
+    result['event_id'] = 'fake-id-'+Math.random().toString(24).substring(2);
+  }
 
-    if (message_text == undefined) {
-        message_text = default_messages[status];
-    }
+  var default_messages = {
+    "success" : "Success, we did what you asked",
+    "info"    : "Here is a basic info update",
+    "debug"   : "This is a simple debug message",
+    "warning" : "This here be a warning to you",
+    "error"   : "You have discovered an error"
+  }
 
-    // Show Message
-    message.find('span.message-text').html(message_text),
-    message.fadeIn();
+  if (result.message == undefined) {
+    result.message = default_messages[result.status];
+  }
 
-    // Complete Action
-    if (complete == undefined) {
+  var notification_data = _.extend(result, {
+    icon: 'icon-message',
+    undo: false
+  });
 
-    } else if (complete == 'hide') {
-        message.delay(5000).fadeOut('normal', function() {
-            message.find('span.message-text').empty();
-        });
-    } else if (complete == 'redirect') {
-        setTimeout(function() { window.location.href = complete_action }, 5000);
-    }
 
-    return function() { message.fadeOut('normal'); };
+  // Add Notification
+  var notification_html = _.template($('#template-notification-bubble').html(), notification_data);
+  $('#notification-bubbles').append(notification_html);
+  setTimeout(function() {
+    $('#event-' + result.event_id).fadeIn('fast');
+  }, 250);
+  
+
+  // Complete Action
+  if (complete == undefined) {
+    setTimeout(function() {
+      $('#event-' + result.event_id).fadeOut(function() {
+        $(this).remove();
+      });
+    }, 7500);
+  } else if (complete == 'hide') {
+      message.delay(5000).fadeOut('normal', function() {
+          message.find('span.message-text').empty();
+      });
+  } else if (complete == 'redirect') {
+      setTimeout(function() {
+        window.location.href = complete_action 
+      }, 5000);
+  }
+
+  return function() { message.fadeOut('normal'); };
 }
 
 
@@ -140,8 +161,9 @@ EventLog.unsubscribe = function(ev, func_or_id) {
 
 
 /* Message Close */
-$(document).on('click', '.message-close', function() {
+$(document).on('click', '.notification-close', function() {
   $(this).parent().fadeOut(function() {
+    $(this).remove();
   });
 });
 
