mailpile/Mailpile

commit 2a5de37c9080dd28565dc4b71b788567349f5d26

Replaced Mailpile.tag_add_delete with updated Mailpile.API.tag_post commands

static/default/html/jsapi/search/content.js
@@ -24,7 +24,7 @@ $(document).on('click', '.pile-tag-delete', function(e) {
   e.preventDefault();
   var tid = $(this).data('tid');
   var mid = $(this).data('mid');
-  Mailpile.tag_add_delete([], tid, mid, function(result) {
+  Mailpile.API.tag_post({ add: [], del: tid, mid: mid}, function(result) {
     $('#pile-message-tag-' + tid + '-' + mid).qtip('hide').remove();
   });
 });

static/default/html/jsapi/search/selection_actions.js
@@ -26,7 +26,7 @@ $(document).on('click', '.bulk-action-tag', function() {
 
 /* Search - Bulk Action - Archive */
 $(document).on('click', '.bulk-action-archive', function() {
-  Mailpile.tag_add_delete('', 'inbox', Mailpile.messages_cache, function() {
+  Mailpile.API.tag_post({ add: '', del: 'inbox', mid: Mailpile.messages_cache}, function() {
 
     // Update Pile View
     $.each(Mailpile.messages_cache, function(key, mid) {
@@ -44,7 +44,7 @@ $(document).on('click', '.bulk-action-archive', function() {
 
 /* Search - Bulk Action - Trash */
 $(document).on('click', '.bulk-action-trash', function() {
-  Mailpile.tag_add_delete('trash', 'new', Mailpile.messages_cache, function() {
+  Mailpile.API.tag_post({ add: 'trash', del: 'new', mid: Mailpile.messages_cache}, function() {
 
     // Update Pile View
     $.each(Mailpile.messages_cache, function(key, mid) {
@@ -62,7 +62,7 @@ $(document).on('click', '.bulk-action-trash', function() {
 
 /* Search - Bulk Action - Spam */
 $(document).on('click', '.bulk-action-spam', function() {
-  Mailpile.tag_add_delete('spam', 'new', Mailpile.messages_cache, function() {
+  Mailpile.API.tag_post({ add: 'spam', del: 'new', mid: Mailpile.messages_cache}, function() {
 
     // Update Pile View
     $.each(Mailpile.messages_cache, function(key, mid) {
@@ -106,7 +106,7 @@ $(document).on('submit', '#form-tag-picker', function(e) {
   }
 
   // Send Result
-   Mailpile.tag_add_delete(add_tags, remove_tags, Mailpile.messages_cache, function(result) {
+   Mailpile.API.tag_post({ add: add_tags, del: remove_tags, mid: Mailpile.messages_cache}, function(result) {
     var tag_link_template = $('#template-search-pile-tags-link').html();
 
     $.each(result.msg_ids, function(key, mid) {

static/default/html/jsapi/ui/sidebar.js
@@ -160,7 +160,7 @@ $(document).ready(function() {
       Mailpile.bulk_cache_add('messages_cache', $(event.target).data('mid'));
 
       // Save Update
-      Mailpile.tag_add_delete(ui.draggable.data('tid'), '', Mailpile.messages_cache, function() {
+      Mailpile.API.tag_post({ add: ui.draggable.data('tid'), del: '', mid: Mailpile.messages_cache}, function() {
 
         var tag = _.findWhere(Mailpile.instance.tags, { tid: ui.draggable.data('tid').toString() });
         var hex = Mailpile.theme.colors[tag.label_color];

static/default/html/jsapi/tags/content.js
@@ -19,27 +19,6 @@ Mailpile.tag_add = function(tag_add, mids, complete) {
 };
 
 
-Mailpile.tag_add_delete = function(tag_add, tag_del, mids, complete) {
-  $.ajax({
-	  url			 : Mailpile.api.tag,
-	  type		 : 'POST',
-	  data     : {
-      add: tag_add,
-      del: tag_del,
-      mid: mids
-    },
-	  dataType : 'json',
-    success  : function(response) {
-      if (response.status == 'success') {
-        complete(response.result);
-      } else {
-        Mailpile.notification(response.status, response.message);
-      }
-    }
-  });
-};
-
-
 Mailpile.tag_update = function(tid, setting, value, complete) {
 
   // Prep Update Value

static/default/html/jsapi/app/drag_drop.js
@@ -30,7 +30,10 @@ Mailpile.sidebar_tags_droppable_opts = {
       var tags_delete = Mailpile.instance.search_tag_ids;
     }
 
-    Mailpile.tag_add_delete(tid, tags_delete, Mailpile.messages_cache, function() {
+    Mailpile.API.tag_post({ add: tid, del: tags_delete, mid: Mailpile.messages_cache}, function(result) {
+
+      console.log(result);
+      Mailpile.notification(result);
 
       // Update Pile View
       if (Mailpile.instance.state.command_url == '/search/') {

static/default/html/jsapi/app/keybindings.js
@@ -7,7 +7,7 @@ Mailpile.keybinding_move_message = function(add_tag) {
     delete_tags.push('new');
 
     // Add / Delete
-    Mailpile.tag_add_delete(add_tag, delete_tags, Mailpile.messages_cache, function() {
+    Mailpile.API.tag_post({ add: add_tag,del: delete_tags, mid: Mailpile.messages_cache}, function() {
 
       // Update Pile View
       $.each(Mailpile.messages_cache, function(key, mid) {

static/default/html/jsapi/message/message.js
@@ -34,7 +34,7 @@ $(document).on('click', '.message-action-forward', function() {
 /* Message - Move message to inbox */
 $(document).on('click', '.message-action-inbox', function() {
   var mid = $(this).parent().parent().parent().parent().data('mid');
-  Mailpile.tag_add_delete(['inbox'], ['spam', 'trash'], mid, function() {
+  Mailpile.API.tag_post({ add: ['inbox'],  del: ['spam', 'trash'], mid: mid}, function() {
     window.location.href = '/in/inbox/';
   });
 });
@@ -43,7 +43,7 @@ $(document).on('click', '.message-action-inbox', function() {
 /* Message - Move message to archive */
 $(document).on('click', '.message-action-archive', function() {
   var mid = $(this).parent().parent().parent().parent().data('mid');
-  Mailpile.tag_add_delete('', ['inbox'], mid, function(response) {
+  Mailpile.API.tag_post({ add: '', del: ['inbox'], mid: mid}, function(response) {
     window.location.href = '/in/inbox/';
   });
 });
@@ -52,7 +52,7 @@ $(document).on('click', '.message-action-archive', function() {
 /* Message - Mark message as spam */
 $(document).on('click', '.message-action-spam', function() {
   var mid = $(this).parent().parent().parent().parent().data('mid');
-  Mailpile.tag_add_delete(['spam'], ['trash', 'inbox'], mid, function() {
+  Mailpile.API.tag_post({ add: ['spam'], del: ['trash', 'inbox'], mid: mid}, function() {
     window.location.href = '/in/inbox/';
   });
 });
@@ -82,7 +82,7 @@ $(document).on('click', '.message-action-unthread', function() {
 /* Message - Move a message to trash */
 $(document).on('click', '.message-action-trash', function() {
   var mid = $(this).parent().parent().data('mid');
-  Mailpile.tag_add_delete(['trash'], ['spam', 'inbox'], mid, function() {
+  Mailpile.API.tag_post({ add: ['trash'], del: ['spam', 'inbox'], mid: mid}, function() {
     window.location.href = '/in/inbox/';
   });
 });

commit bb46611e400b0f18261059338642d14f8819529c

Fix things broken by 2a5de37c (tag_add_delete/tag_post change)

static/default/html/jsapi/search/bulk_actions.js
@@ -16,7 +16,7 @@ Mailpile.bulk_actions_update_ui = function() {
 
 
 Mailpile.bulk_action_read = function() {
-  this.tag_add_delete(Mailpile.tags_cache, 'new', Mailpile.messages_cache, function(result) {
+  Mailpile.API.tag_post({ del: 'new', mid: Mailpile.messages_cache }, function(result) {
     $.each(Mailpile.messages_cache, function(key, mid) {
       $('#pile-message-' + mid).removeClass('in_new');
     });
@@ -25,7 +25,7 @@ Mailpile.bulk_action_read = function() {
 
 
 Mailpile.bulk_action_unread = function() {
-  this.tag_add_delete('new', Mailpile.tags_cache, Mailpile.messages_cache, function(result) {
+  Mailpile.API.tag_post({ add: 'new', mid: Mailpile.messages_cache }, function(result) {
     $.each(Mailpile.messages_cache, function(key, mid) {
       $('#pile-message-' + mid).addClass('in_new');
     });

static/default/html/jsapi/search/content.js
@@ -24,7 +24,7 @@ $(document).on('click', '.pile-tag-delete', function(e) {
   e.preventDefault();
   var tid = $(this).data('tid');
   var mid = $(this).data('mid');
-  Mailpile.API.tag_post({ add: [], del: tid, mid: mid}, function(result) {
+  Mailpile.API.tag_post({ del: tid, mid: mid }, function(result) {
     $('#pile-message-tag-' + tid + '-' + mid).qtip('hide').remove();
   });
 });

static/default/html/jsapi/search/selection_actions.js
@@ -26,7 +26,7 @@ $(document).on('click', '.bulk-action-tag', function() {
 
 /* Search - Bulk Action - Archive */
 $(document).on('click', '.bulk-action-archive', function() {
-  Mailpile.API.tag_post({ add: '', del: 'inbox', mid: Mailpile.messages_cache}, function() {
+  Mailpile.API.tag_post({ del: 'inbox', mid: Mailpile.messages_cache}, function() {
 
     // Update Pile View
     $.each(Mailpile.messages_cache, function(key, mid) {

static/default/html/jsapi/ui/sidebar.js
@@ -160,7 +160,7 @@ $(document).ready(function() {
       Mailpile.bulk_cache_add('messages_cache', $(event.target).data('mid'));
 
       // Save Update
-      Mailpile.API.tag_post({ add: ui.draggable.data('tid'), del: '', mid: Mailpile.messages_cache}, function() {
+      Mailpile.API.tag_post({ add: ui.draggable.data('tid'), mid: Mailpile.messages_cache }, function() {
 
         var tag = _.findWhere(Mailpile.instance.tags, { tid: ui.draggable.data('tid').toString() });
         var hex = Mailpile.theme.colors[tag.label_color];