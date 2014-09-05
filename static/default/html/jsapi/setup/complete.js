36d23f6a7c71526dfade9a8889a88c3ed4520869
Brennan Novak <hi@brennannovak.com>
2014-09-05 14:38:52 +0000

    Fixed spelling, added more copy & diversity to Complete stage

--- static/default/html/jsapi/setup/complete.js
+++ static/default/html/jsapi/setup/complete.js
@@ -11,55 +11,90 @@ var CompleteModel = Backbone.Model.extend({
       '{{_("Copying mail. Put your arms above your head and whistle la cucaracha")}}',
       '{{_("Making a copy of your mail. Putting it in your inbox, all comfy like")}}'
     ],
+    nerds: [
+      '{{_("Damn kids. Theyre all alike.")}}',
+      '{{_("RMS approves!")}}',
+      '{{_("Formating your C:\ drive")}}',
+      '{{_("Fortifying encryption shields")}}',
+      '{{_("Increasing entropy & scrambling bits")}}',
+      '{{_("Patching bugs...")}}',
+      '{{_("Indexing kittens...")}}',
+      '{{_("Reticulating Splines")}}',
+      '{{_("Syntax error in line 45 of this e-mail")}}',
+      '{{_("Shoveling more coal into the server")}}',
+      '{{_("Calibrating flux capacitors")}}',
+      '{{_("A few bytes tried to escape, but we caught them")}}',
+      '{{_("Deterministically simulating future state")}}',
+      '{{_("Embiggening prototypes")}}',
+      '{{_("Resolving interdependence")}}',
+      '{{_("Spinning violently around the y-axis")}}',
+      '{{_("Locating additional gigapixels")}}',
+      '{{_("Initializing hamsters")}}',
+      '{{_("This is our world now... the world of the electron and the switch")}}',
+      '{{_("Uploading ad keywords to advertisers... doh!")}}',
+      '{{_("Becoming self-aware")}}',
+      '{{_("Looking for heretofore unknown prime numbers")}}',
+      '{{_("Re-aligning satellite grid")}}',
+      '{{_("Re-routing bitstream")}}',
+      '{{_("Warming up particle accelerator")}}',
+      '{{_("Time is an illusion. Loading time doubly so")}}',
+      '{{_("Verifying local gravitational constant")}}',
+      '{{_("This server is powered by a lemon and two electrodes")}}'
+    ],
     email: [
-      '{{_("Mailpile has an advanced tagging system, use Tags to organize and search your mail")}}',
+      '{{_("Mailpile has an advanced tagging system & search engine at its core")}}',
       '{{_("Do you really need to keep all these Amazon Prime shipping confirmations?")}}',
       '{{_("I am pretty sure email from ThinkGeeks 2011 X-mas sale can be deleted.")}}',
       '{{_("Do you really need an email to know when you have been retweeted?")}}',
       '{{_("Email is the largest internet based social network on the planet")}}',
       '{{_("Which other technology do you use that is 40 years old?")}}',
-      '{{_("There are 2.5 billion email users worldwide, thats double the amount of Facebook users!")}}'  
+      '{{_("There are 2.5 billion email users worldwide, thats double the amount of Facebook users!")}}',
+      '{{_("Use Mailpile Tags to better organize and search your mail")}}',
+      '{{_("Remember getting your first email address? Remember how it felt like something private?")}}',
+      '{{_("Email is a decentralized by design, this means no one company or government owns it!")}}',
+      '{{_("Over 100 trillion emails are sent per year, wow!")}}',
+      '{{_("Email is most widely used direct communication protocol every created by humans")}}',
+      '{{_("Email is based on an open standard agreed upon by the entire world and owned by no one")}}'
     ],
     security: [
-      '{{_("Mailpile uses OpenGPG / PGP to encrypt and decrypt your messages securely")}}',
+      '{{_("BCC-ing NSA")}}',
+      '{{_("Sending encrypted mail to Snowden")}}',
+      '{{_("Decrypting an email from Snowden")}}',
+      '{{_("The worlds most powerful governments are conducting mass dragnet surveillance")}}',
+      '{{_("Most email can be read by anyone as it travel through the internet")}}',
+      '{{_("Encryption ensures that your emails are only read by the intended recipient")}}',
+      '{{_("Unencrypted email is more like sending a postcard than sending a letter")}}',
+      '{{_("Mailpile uses OpenPGP to encrypt and decrypt your messages securely")}}',
+      '{{_("All of your config settings & passwords are encrypted with AES 256")}}',
       '{{_("Encrypting emails means your communication actually stays private")}}',
-      '{{_("The more encrypted email you send, the better!")}}'
+      '{{_("The more encrypted email you send, the better!")}}',
+      '{{_("Make sure you print or save your keys & passphrase somewhere securely")}}',
+      '{{_("Mailpile by default encrypts your search index!")}}',
+      '{{_("The most common Email password is 123456, hopefully yours is different ;)")}}'
     ],
     jokes: [
       '{{_("Good things come to those who wait")}}',
       '{{_("Make free software and be happy")}}',
       '{{_("Most of Mailpile is made in Iceland, where much of the country believes in elves")}}',
       '{{_("The founders of Mailpile met in a public hot tub in Reykjavík")}}',
-      '{{_("Damn kids. Theyre all alike.")}}',
       '{{_("We like volcanos, do you like volcanos?")}}',
       '{{_("A million hamsters are spinning their wheels right now")}}',
       '{{_("Tapping earth for more geothermal energy")}}',
-      '{{_("Fortifying encryption shields")}}',
-      '{{_("Increasing entropy & scrambling bits")}}',
       '{{_("Digging moat. Filing with alligators. Fortifying walls")}}',
       '{{_("Indexing lovenotes")}}',
-      '{{_("Uploading ad keywords to advertisers... doh!")}}',
       '{{_("Crossing out swear words...")}}',
       '{{_("Compiling bullshit bingo grid...")}}',
       '{{_("Abandon all hope, ye who enter here")}}',
       '{{_("Welcome to the nine circles of suffering")}}',
       '{{_("Mailing wife about female contacts")}}',
-      '{{_("BCC-ing NSA")}}',
       '{{_("Informing authorities of suspicious activities")}}',
-      '{{_("Decrypting an email from Snowden")}}',
-      '{{_("Sending encrypted mail to Snowden")}}',
-      '{{_("RMS approves!")}}',
-      '{{_("Formating your C:\ drive")}}',
       '{{_("Letting you wait for no apparent reason")}}',
       '{{_("What are you wearing?")}}',
       '{{_("Go put the kettle on, this could be a while")}}',
       '{{_("Go get a cup of tea and some biscuits. This will take approximately 4 custard creams worth of time")}}',
       '{{_("Did you know humans share 50 percent of their DNA with a banana? Perverts")}}',
-      '{{_("Reticulating Splines")}}',
       '{{_("Estimating chance of astroid hitting Earth")}}',
-      '{{_("Looking for heretofore unknown prime numbers")}}',
       '{{_("Reading Terms of Service documents")}}',
-      '{{_("Becoming self-aware")}}',
       '{{_("Catching up on shows on Netflix")}}',
       '{{_("Oh, you have some very interesting stuff there")}}',
       '{{_("I think I better understand you better now")}}',
@@ -68,13 +103,10 @@ var CompleteModel = Backbone.Model.extend({
       '{{_("Applying coupons...")}}',
       '{{_("Licking stamps...")}}',
       '{{_("Self potato")}}',
-      '{{_("Syntax error in line 45 of this e-mail")}}',
       '{{_("Yum yum, that one was tasty")}}',
       '{{_("Hey, there is some Nigerian prince here who wants to give you twenty million dollars...")}}',
       '{{_("How rude!")}}',
-      '{{_("Patching bugs...")}}',
-      '{{_("Indexing kittens...")}}',
-      '{{_("Enhancing photos")}}',
+      '{{_("Now enhancing photos")}}',
       '{{_("Backing up the entire Internet...")}}',
       '{{_("Really? You are still waiting?")}}',
       '{{_("Well... it sure is a beautiful day")}}',
@@ -83,28 +115,13 @@ var CompleteModel = Backbone.Model.extend({
       '{{_("Doing nothing")}}',
       '{{_("Making you wait for no reason")}}',
       '{{_("Testing your patience")}}',
-      '{{_("Locating some gigapixels")}}',
-      '{{_("Initializing hamsters")}}',
-      '{{_("Shoveling more coal into the server")}}',
-      '{{_("Calibrating flux capacitors")}}',
       '{{_("Pay no attention to the man behind the curtain")}}',
-      '{{_("A few bytes tried to escape, but we caught them")}}',
-      '{{_("Verifying local gravitational constant")}}',
-      '{{_("This server is powered by a lemon and two electrodes")}}',
       '{{_("You are great just the way you are")}}',
       '{{_("Warning: do not think of purple hippos")}}',
       '{{_("Follow the white rabbit")}}',
       '{{_("Wanna see how deep the rabbit hole goes?")}}',
-      '{{_("Re-aligning satellite grid")}}',
-      '{{_("Re-routing bitstream")}}',
       '{{_("Supplying monkeys with typewriters")}}',
-      '{{_("Warming up particle accelerator")}}',
-      '{{_("Time is an illusion. Loading time doubly so")}}',
       '{{_("Waiting for Godot.")}}',
-      '{{_("Deterministically simulating future state")}}',
-      '{{_("Embiggening prototypes")}}',
-      '{{_("Resolving interdependence")}}',
-      '{{_("Spinning violently around the y-axis")}}',
       '{{_("Swapping time and space")}}'
     ],
     state: 0
@@ -112,16 +129,18 @@ var CompleteModel = Backbone.Model.extend({
   states: {
     0: 'complete',
     1: 'copying',
-    2: 'email',
-    3: 'security',
-    4: 'jokes'
+    2: 'nerds',
+    3: 'email',
+    4: 'security',
+    5: 'jokes'
   },
   icons: {
     0: 'icon-like',
     1: 'icon-inbox',
-    2: 'icon-message',
-    3: 'icon-lock-closed',
-    4: 'icon-star'
+    2: ['icon-trophy', 'icon-robot', 'icon-graph'],
+    3: ['icon-message', 'icon-tag', 'icon-compose'],
+    4: ['icon-privacy', 'icon-lock-closed', 'icon-key'],
+    5: ['icon-star', 'icon-lightbulb', 'icon-new']
   }
 });
 
@@ -173,21 +192,28 @@ var CompleteView = Backbone.View.extend({
     var last = CompleteView.model.get('state');
     var now = 0;
 
-    if (last < 4) {
+    if (last < 5) {
       now = last;
       now++;
     } else {
-      last = 4;
+      last = 5;
       now = 1;
     }
 
     CompleteView.model.set({ state: now });
 
-    var state_now  = CompleteView.model.states[now];
-    var message = _.sample(CompleteView.model.attributes[state_now]);
+    var state_now = CompleteView.model.states[now];
+    var message   = _.sample(CompleteView.model.attributes[state_now]);
 
     var icon_last = CompleteView.model.icons[last];
     var icon_now  = CompleteView.model.icons[now];
+
+    if (_.isArray(icon_last)) {
+      icon_last = icon_last.join(' ');
+    }
+    if (_.isArray(icon_now)) {
+      icon_now = _.sample(icon_now);
+    }
 
     // Update with live data
     var copying = 'Copying [LIVE DATA] Mail';
