1cff0200c45de86f577a98d7e142cf4741f2c707
Brennan Novak <hi@brennannovak.com>
2014-10-12 00:00:26 +0200

    Added rough support for Typehead in search bar

--- static/default/html/jsapi/app/activities.js
+++ static/default/html/jsapi/app/activities.js
@@ -12,3 +12,45 @@ Mailpile.activities.compose = function(address) {
     }
   });
 };
+
+
+Mailpile.activities.render_typeahead = function() {
+
+  var substringMatcher = function(strs) {
+    return function findMatches(q, cb) {
+      var matches, substrRegex;
+  
+      // an array that will be populated with substring matches
+      matches = [];
+  
+      // regex used to determine if a string contains the substring `q`
+      substrRegex = new RegExp(q, 'i');
+  
+      // iterate through the pool of strings and for any string that
+      // contains the substring `q`, add it to the `matches` array
+      $.each(strs, function(i, str) {
+        if (substrRegex.test(str)) {
+          // the typeahead jQuery plugin expects suggestions to a
+          // JavaScript object, refer to typeahead docs for more info
+          matches.push({ value: str });
+        }
+      });
+  
+      cb(matches);
+    };
+  };
+
+  // List of basic suggestions for search helpers
+  var helpers = ['to: ', 'from: ', 'subject: ', 'in: ', 'contacts: ', 'tags: ', 'keys: '];
+
+  $('#form-search .typeahead').typeahead({
+    hint: true,
+    highlight: true,
+    minLength: 0
+  },{
+    name: 'helpers',
+    displayKey: 'value',
+    source: substringMatcher(helpers)
+  });
+
+};
