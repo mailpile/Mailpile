07ba6268b541eb2878c60a507f124a4f4aafd023
Brennan Novak <hi@brennannovak.com>
2014-10-29 23:32:50 +0100

    Added new .show-subtags clases to SubtagToggle JS

--- static/default/html/jsapi/ui/sidebar.js
+++ static/default/html/jsapi/ui/sidebar.js
@@ -2,12 +2,14 @@ Mailpile.UI.Sidebar.SubtagsToggle = function(tid) {
 
   // Show or Hide
   if (_.indexOf(Mailpile.config.web.subtags_collapsed, tid) > -1) {
+    $('#sidebar-tag-' + tid).addClass('show-subtags');
     $('#sidebar-tag-' + tid).find('a.sidebar-tag-expand span').removeClass('icon-expand').addClass('icon-collapse');
-    $('#sidebar-subtags-' + tid).slideDown();
+    $('#sidebar-subtags-' + tid).slideDown('fast');
     var collapsed = _.without(Mailpile.config.web.subtags_collapsed, tid);
   } else {
+    $('#sidebar-tag-' + tid).removeClass('show-subtags');
     $('#sidebar-tag-' + tid).find('a.sidebar-tag-expand span').removeClass('icon-collapse').addClass('icon-expand');
-    $('#sidebar-subtags-' + tid).slideUp();
+    $('#sidebar-subtags-' + tid).slideUp('fast');
     Mailpile.config.web.subtags_collapsed.push(tid);
     var collapsed = Mailpile.config.web.subtags_collapsed;
   }
