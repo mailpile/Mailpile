1c05d58d68d3e8f57165c2562896081a2292c0e3
Brennan Novak <hi@brennannovak.com>
2014-11-22 00:40:07 +0100

    disallowed adding of RFC5322 compliant e-mail address to composer

--- static/default/html/jsapi/compose/recipients.js
+++ static/default/html/jsapi/compose/recipients.js
@@ -2,6 +2,7 @@
 
 Mailpile.Composer.Recipients.AnalyzeAddress = function(address) {
   var check = address.match(/([^<]+?)\s<(.+?)(#[a-zA-Z0-9]+)?>/);
+
   if (check) {
     if (check[3]) {
       return {"id": check[2], "fn": $.trim(check[1]), "address": check[2], "keys": [{ "fingerprint": check[3].substring(1) }], "flags": { "secure" : true } };
@@ -94,9 +95,6 @@ Mailpile.Composer.Recipients.AddressField = function(id) {
     createSearchChoice: function(term) {
       // Check if we have an RFC5322 compliant e-mail address
       if (term.match(/(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/)) {
-        return {"id": term, "fn": term, "address": term, "flags": { "secure" : false }};
-      // FIXME: handle invalid email addresses with UI feedback
-      } else {
         return {"id": term, "fn": term, "address": term, "flags": { "secure" : false }};
       }
     },
