4beec7191287ebc2fad6417373ac6a9bf5ec84c3
Bjarni R. Einarsson <bre@klaki.net>
2014-12-30 16:57:55 +0100

    Fix buglets in the command cache (which broke jhtml generation)

--- mailpile/command_cache.py
+++ mailpile/command_cache.py
@@ -34,59 +34,82 @@ class CommandCache(object):
     def __init__(self, debug=None):
         self.debug = debug or (lambda s: None)
         self.lock = UiRLock()
-        self.cache = {}     # The cache itself
-        self.dirty = set()  # Requirements that have changed recently
+        self._lag = 0.5
+        self.cache = {}     # id -> [exp, req, ss, cmd_obj, res_obj, added]
+        self.dirty = []     # (ts, req): Requirements that changed & when
 
     def cache_result(self, fprint, expires, req, cmd_obj, result_obj):
         with self.lock:
             # Make a snapshot of the session, as it provides context
-            snapshot = Session.Snapshot(cmd_obj.session, ui=False)
-            snapshot.ui = BackgroundInteraction(cmd_obj.session.config,
-                                                log_parent=cmd_obj.session.ui)
-            cmd_obj.session = result_obj.session = snapshot
+            ss = Session.Snapshot(cmd_obj.session, ui=False)
+            ss.ui = BackgroundInteraction(cmd_obj.session.config,
+                                          log_parent=cmd_obj.session.ui)
 
             # Note: We cache this even if the requirements are "dirty",
             #       as mere presence in the cache makes this a candidate
             #       for refreshing.
-            self.cache[str(fprint)] = [expires, req, cmd_obj, result_obj]
+            self.cache[str(fprint)] = [expires, req, ss, cmd_obj, result_obj,
+                                       time.time()]
             self.debug('Cached %s, req=%s' % (fprint, req))
 
-    def get_result(self, fprint, dirty_check=True):
+    def get_result(self, fprint, dirty_check=True, extend=60):
+        max_a = time.time() - self._lag
         with self.lock:
-            exp, req, co, result_obj = match = self.cache[fprint]
-            match[0] += 60
-        if dirty_check and req & self.dirty:
-            # If requirements are dirty, pretend this item does not exist.
-            raise KeyError()
+            exp, req, ss, co, result_obj, a = match = self.cache[fprint]
+        if (a >= max_a) or (dirty_check and req & self.dirty_set(after=a)):
+            # If item is too new, or requirements are dirty, pretend this
+            # item does not exist.
+            raise KeyError(fprint)
+        match[0] = min(match[0] + extend, time.time() + 5 * extend)
+        co.session = result_obj.session = ss
         return result_obj
 
+    def dirty_set(self, after=0):
+        dirty = set()
+        with self.lock:
+            for ts, req in self.dirty:
+                if after == 0 or ts > after:
+                    dirty |= req
+        return dirty
+
     def mark_dirty(self, requirements):
-        self.dirty |= set(requirements)
+        with self.lock:
+            self.dirty.append((time.time(), set(requirements)))
         self.debug('Marked dirty: %s' % requirements)
 
-    def refresh(self, extend=60, runtime=4, event_log=None):
-        now = time.time()
+    def refresh(self, extend=0, runtime=4, event_log=None):
+        started = now = time.time()
         with self.lock:
+            # Expire things from the cache
             expired = set([f for f in self.cache if self.cache[f][0] < now])
             for fp in expired:
                 del self.cache[fp]
 
-            dirty, self.dirty = self.dirty, set()
+            # Expire things from the dirty set
+            self.dirty = [(ts, req) for ts, req in self.dirty
+                          if ts > (now - self._lag)]
+
+            # Decide which fingerprints to look at this time around
             fingerprints = list(self.cache.keys())
 
         refreshed = []
         for fprint in fingerprints:
             try:
-                exp, req, co, ro = self.cache[fprint]
-                if req & dirty:
-                    if time.time() < now + runtime:
+                e, req, ss, co, ro, a = self.cache[fprint]
+                now = time.time()
+                if (a + self._lag) < now and req & self.dirty_set(after=a):
+                    if now < started + runtime:
+                        co.session = ro.session = ss
                         ro = co.refresh()
+                        if extend > 0:
+                            e = min(e + extend, now + 5*extend)
                         with self.lock:
-                            self.cache[fprint] = [exp + extend, req, co, ro]
+                            self.cache[fprint] = [e, req, ss, co, ro, now]
                         refreshed.append(fprint)
                         play_nice_with_threads()
                     else:
-                        # Out of time, just evict things.
+                        # Out of time, evict because otherwise it may be
+                        # assumed to be up-to-date.
                         with self.lock:
                             del self.cache[fprint]
             except (ValueError, IndexError, TypeError):
