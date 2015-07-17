c6775023458a81075b5e26f5b71cf539a85a2a35
Bjarni R. Einarsson <bre@klaki.net>
2015-07-17 01:09:42 +0000

    Add transifex config, make message compiler more stubborn

--- scripts/compile-messages.sh
+++ scripts/compile-messages.sh
@@ -3,5 +3,5 @@ set -e
 cd "$(dirname $0)"/..
 for L in $(find mailpile/locale -type d |grep "LC_MESSAGES"); do
     echo msgfmt $L/mailpile.po -o $L/mailpile.mo
-    msgfmt $L/mailpile.po -o $L/mailpile.mo
+    msgfmt $L/mailpile.po -o $L/mailpile.mo || rm -f $L/mailpile.mo
 done;
