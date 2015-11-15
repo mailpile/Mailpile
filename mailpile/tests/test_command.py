mailpile/Mailpile

commit abb3a9168e492c81891961f34307b5f53737bd6f

Do not add consecutive "Re:" or "Fw:" on subjects
Avoid adding a subject prefix which is already present on refered message while
forwarding/replying a message.

It handles common english-lang forms of Response/Forward tags.

mailpile/plugins/compose.py
@@ -377,6 +377,17 @@ class RelativeCompose(Compose):
     _TEXT_PARTTYPES = ('text', 'quote', 'pgpsignedtext', 'pgpsecuretext',
                        'pgpverifiedtext')
 
+    _FW_REGEXP = re.compile(r'^(fwd|fw):.*', re.IGNORECASE)
+    _RE_REGEXP = re.compile(r'^(rep|re):.*', re.IGNORECASE)
+
+    @staticmethod
+    def prefix_subject(subject, prefix, prefix_regex):
+        """Avoids stacking several consecutive Fw: Re: Re: Re:"""
+        if prefix_regex.match(subject):
+            return subject
+        else:
+            return '%s %s' % (prefix, subject)
+
 
 class Reply(RelativeCompose):
     """Create reply(-all) drafts to one or more messages"""
@@ -507,7 +518,8 @@ class Reply(RelativeCompose):
 
         return (Email.Create(idx, local_id, lmbox,
                              msg_text='\n\n'.join(msg_bodies),
-                             msg_subject=('Re: %s' % ref_subjs[-1]),
+                             msg_subject=cls.prefix_subject(
+                                 ref_subjs[-1], 'Re:', cls._RE_REGEXP),
                              msg_from=headers.get('from', None),
                              msg_to=headers.get('to', []),
                              msg_cc=headers.get('cc', []),
@@ -609,7 +621,8 @@ class Forward(RelativeCompose):
 
         email = Email.Create(idx, local_id, lmbox,
                              msg_text='\n\n'.join(msg_bodies),
-                             msg_subject=('Fwd: %s' % ref_subjs[-1]),
+                             msg_subject=cls.prefix_subject(
+                                 ref_subjs[-1], 'Fwd:', cls._FW_REGEXP),
                              msg_id=msgid,
                              msg_atts=msg_atts,
                              save=(not ephemeral),

mailpile/tests/test_command.py
@@ -71,6 +71,46 @@ class TestCommands(MailPileUnittest):
         self.assertEqual(res.as_dict()["result"]['crypto-policy'],
                          'best-effort')
 
+    def test_reply_subjects_on_first_msg(self):
+        # Subject: Verb. Target. Outcome.'
+        mid = self.mp.search('subject:Verb').result['data']['metadata']\
+                                            .values()[0]['mid']
+        res = self.mp.reply('ephemeral', '=%s' % mid)
+        self.assertEqual(res.result['data']['metadata'].values()[0]['subject'],
+                         'Re: Verb. Target. Outcome.')
+
+    def test_reply_subject_on_reply_msg(self):
+        # Subject: Re: Here's $1
+        mid = self.mp.search('subject:Here').result['data']['metadata']\
+                                            .values()[0]['mid']
+        res = self.mp.reply('ephemeral', '=%s' % mid)
+        self.assertEqual(res.result['data']['metadata'].values()[0]['subject'],
+                         "Re: Here's $1")
+
+    def test_reply_subject_on_fw_msg(self):
+        # Subject: Fw: About shrubberies
+        mid = self.mp.search('shrubberies').result['data']['metadata']\
+                                           .values()[0]['mid']
+        res = self.mp.reply('ephemeral', '=%s' % mid)
+        self.assertEqual(res.result['data']['metadata'].values()[0]['subject'],
+                         "Re: Fw: About shrubberies")
+
+    def test_fwd_subject_on_re_msg(self):
+        # Subject: Re: Here's $1
+        mid = self.mp.search('subject:Here').result['data']['metadata']\
+                                            .values()[0]['mid']
+        res = self.mp.forward('ephemeral', '=%s' % mid)
+        self.assertEqual(res.result['data']['metadata'].values()[0]['subject'],
+                         "Fwd: Re: Here's $1")
+
+    def test_fw_subject_on_fw_msg(self):
+        # Subject: Fw: A question shrubberies
+        mid = self.mp.search('shrubberies').result['data']['metadata']\
+                                           .values()[0]['mid']
+        res = self.mp.forward('ephemeral', '=%s' % mid)
+        self.assertEqual(res.result['data']['metadata'].values()[0]['subject'],
+                         "Fw: About shrubberies")
+
 
 class TestCommandResult(MailPileUnittest):
     def test_command_result_as_dict(self):

mailpile/tests/__init__.py
@@ -13,12 +13,15 @@ import mailpile.util
 from mailpile.plugins.tags import AddTag, Filter
 from mailpile.crypto.gpgi import GNUPG_HOMEDIR
 from mailpile.ui import SilentInteraction
+from mailpile.vcard import AddressInfo
 
 # Pull in all the standard plugins, plus the demos.
 from mailpile.mailboxes import *
 from mailpile.plugins import *
 
 MP = None
+MY_FROM = 'team+testing@mailpile.is'
+MY_NAME = 'Mailpile Team'
 
 
 def get_mailpile_root():
@@ -63,6 +66,7 @@ def _initialize_mailpile_for_testing(workdir, test_data):
 
     mp.add(test_data)
     mp.rescan('mailboxes')
+    mp.profiles_add(MY_FROM, '=', MY_NAME)
 
     return mp, session, config, ui
 

mailpile/tests/data/Maildir/cur/fw-question-1443197278.mbx
@@ -1,0 +1,8 @@
+From nobody Fri Mar  7 15:17:43 2014
+Content-Type: text/plain; charset=UTF-8
+MIME-Version: 1.0
+Subject: Fw: About shrubberies
+From: you@test.local
+To: another@test.local
+
+Test

mailpile/tests/data/Maildir/cur/mailpile-1400069992.mbx
@@ -34,7 +34,7 @@ To: hi@wigglebot.com
 Cc: cash@square.com
 X-DynectEmail-Msg-Key: 20140514011619.FF8A06700036@mail6-12-ewr
 Message-ID:  <f38s7nk064or5h7pjjwzsj4kt-a881f968-36a7-4af7-b027-a12d6b686326@square.com>
-Subject: Here's $1
+Subject: Re: Here's $1
 MIME-Version: 1.0
 Content-Type: multipart/alternative;  boundary="----=_Part_31399_1772714090.1400030175154"
 X-Square-Cash-Referrer-Token: C_apf4xbdgjbf39ema8ie8noen3

mailpile/tests/test_search.py
@@ -21,7 +21,7 @@ def checkSearch(query, expected_count=1):
 
 def test_generator():
     # All mail
-    yield checkSearch(['all:mail'], 9)
+    yield checkSearch(['all:mail'], 10)
     # Full match
     yield checkSearch(['brennan'])
     # Partial match

commit eef1a6a827c76d83c6c065aebf7f02ce5414e645

Fix crash replying to a subject-less message
I think I introduced it in abb3a9168e492c81891961f34307b5f53737bd6f

mailpile/plugins/compose.py
@@ -383,7 +383,9 @@ class RelativeCompose(Compose):
     @staticmethod
     def prefix_subject(subject, prefix, prefix_regex):
         """Avoids stacking several consecutive Fw: Re: Re: Re:"""
-        if prefix_regex.match(subject):
+        if subject is None:
+            return prefix
+        elif prefix_regex.match(subject):
             return subject
         else:
             return '%s %s' % (prefix, subject)

mailpile/tests/data/Maildir/cur/no-subject-1400067892.mbx
@@ -1,0 +1,7 @@
+From nobody Fri Mar  7 15:17:43 2014
+Content-Type: text/plain; charset=UTF-8
+MIME-Version: 1.0
+From: ohcheeou@test.local
+To: another@test.local
+
+Test

mailpile/tests/test_command.py
@@ -71,6 +71,15 @@ class TestCommands(MailPileUnittest):
         self.assertEqual(res.as_dict()["result"]['crypto-policy'],
                          'best-effort')
 
+    def test_reply_no_subject(self):
+        mid = self.mp.search('from:ohcheeou').result['data']['metadata']\
+                                             .values()[0]['mid']
+        # Just checks it does not crash
+        res = self.mp.reply('ephemeral', '=%s' % mid)
+        self.assertEqual(res.status, 'success')
+        self.assertEqual(res.result['data']['metadata'].values()[0]['subject'],
+                         'Re:')
+
     def test_reply_subjects_on_first_msg(self):
         # Subject: Verb. Target. Outcome.'
         mid = self.mp.search('subject:Verb').result['data']['metadata']\