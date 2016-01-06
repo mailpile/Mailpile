e2e8890f761431866c094239d9680be123aae864
Bjarni R. Einarsson <bre@klaki.net>
2016-01-06 13:55:19 +0000

    Add organization details to generated .pot file, fixes #1518

--- scripts/make-messages.sh
+++ scripts/make-messages.sh
@@ -5,8 +5,14 @@ cd "$(dirname $0)"/..
 
 pybabel extract --project=mailpile \
     -F babel.cfg \
-    -o mailpile/locale/mailpile.pot \
+    -o mailpile/locale/mailpile.pot.tmp \
     .
+
+sed -e 's/ORGANIZATION/Mailpile ehf/' \
+    -e 's/FIRST AUTHOR <EMAIL@ADDRESS>/Mailpile Team <team@mailpile.is>/' \
+    < mailpile/locale/mailpile.pot.tmp \
+    > mailpile/locale/mailpile.pot \
+    && rm -f mailpile/locale/mailpile.pot.tmp
 
 for L in $(find mailpile/locale -type d |grep "LC_MESSAGES"); do
     msgmerge -U $L/mailpile.po mailpile/locale/mailpile.pot
