8d9f939ef82203f8eb42dda3e4825aad614a9434
Jack <brmdamon>
2016-09-26 12:50:36 -0400

    Simplify code for removal of xml encoding declaration.

--- mailpile/mailutils.py
+++ mailpile/mailutils.py
@@ -963,7 +963,8 @@ class Email(object):
                 return mbox, msg_ptr, FixupForWith(fd)
             except (IOError, OSError, KeyError, ValueError, IndexError):
                 # FIXME: If this pointer is wrong, should we fix the index?
-                print 'WARNING: %s not found' % msg_ptr
+                if 'sources' in self.config.sys.debug:
+                    print 'WARNING: %s not found' % msg_ptr
         return None, None, None
 
     def get_file(self):
@@ -1347,18 +1348,16 @@ class Email(object):
 
                 if mimetype == 'text/html':
                     if want is None or 'html_parts' in want:
+
                         # Find and delete possibly conflicting xml encoding
                         # declaration to prevent lxml ValueError.
                         # e.g. <?xml version="1.0" encoding="ISO-8859-1"?>
-                        re_encoding=re.compile('<\?xml version=[^ ?>]*'+
-                                                '((?! +encoding=) [^ ?>]*)*'+
-                                                '( +encoding=[^ ?>]*)')
-                        enc_match=re_encoding.match(payload)
-                        if enc_match:
-                            enc_beg=enc_match.span(2)[0]
-                            enc_end=enc_match.span(2)[1]
-                            if enc_beg>0 and enc_end>enc_beg:
-                               payload=payload[:enc_beg]+payload[enc_end:]
+                        payload = re.sub( '(<\?xml version=[^ ?>]*'+
+                                            '((?! +encoding=) [^ ?>]*)*)'+
+                                            '( +encoding=[^ ?>]*)',
+                                            r'\1',
+                                            payload )
+ 
                         tree['html_parts'].append({
                             'charset': charset,
                             'type': 'html',
