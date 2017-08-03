5762ab5bfebe243bdf43b4e18426683deb4bdf8d
Bjarni R. Einarsson <bre@klaki.net>
2017-08-03 17:24:19 +0000

    Call this 1.0.0rc1

--- mailpile/config/defaults.py
+++ mailpile/config/defaults.py
@@ -1,4 +1,4 @@
-APPVER = "1.0.0rc0"
+APPVER = "1.0.0rc1"
 ABOUT = """\
 Mailpile.py              a tool             Copyright 2013-2017, Mailpile ehf
  v%8.0008s         for searching and               <https://www.mailpile.is/>
