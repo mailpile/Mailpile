60bc3b520bd004cfadfe25aa07c8dfc99b451501
Jack <brmdamon>
2017-11-07 12:04:07 -0500

    Working Windows installer

--- packages/windows/repackage-linux.sh
+++ packages/windows/repackage-linux.sh
@@ -1,78 +1,296 @@
+# This script makes a Mailpile Windows installer from the Mailpile source code,
+# Python distribution files and other source code, which are all downloaded from
+# the "official" Internet site of each software provider. 
 #
-# This is a script which will repackage Mailpile for distribution on
-# Windows.  Tested on Ubuntu 14.04, YMMV.
-#
+# This has been tested on Debian Stretch.
+# Required Debian packages (this list may be incomplete):
+# git gnupg gzip mingw-w64 nsis openssl p7zip python2.7 python-pip tar unzip
+# wget xz-utils (? archiver used by GnuPG build?)   
+# 
+# Copyright (C) 2017 Jack Dodds
+# This script is part of Mailpile and is hereby released under the
+# Gnu Affero Public Licence v.3 - see ../../COPYING and ../../AGPLv3.txt.
+
+REQUIREMENTS="requirements-with-deps.txt"
+
+MAILPILE_GIT="https://github.com/mailpile/Mailpile.git"
+MAILPILE_BRANCH=master
+
+PYTHON_SITE="https://www.python.org/ftp/python/2.7.14/"
+PYTHON_FILE="python-2.7.14.msi"
+# Checksum from https://www.python.org/downloads/release/python-2714  No SHA256!
+PYTHON_FILE_MD5=fff688dc4968ec80bbb0eedf45de82db
+
+OPENSSL_SITE="https://www.openssl.org/source"
+OPENSSL_FILE="openssl-1.1.0f.tar.gz"
+# Checksum from https://www.openssl.org/source/openssl-1.1.0f.tar.gz.sha256
+OPENSSL_FILE_SHA256=\
+12f746f3f2493b2f39da7ecf63d7ee19c6ac9ec6a4fcd8c229da8a522cb12765
+
+GNUPG_SITE="https://www.gnupg.org/ftp/gcrypt/gnupg"
+GNUPG_FILE="gnupg-2.2.1.tar.bz2"
+# Checksum from https://gnupg.org/download/integrity_check.html. Why no SHA-256?
+GNUPG_FILE_SHA1=5455373fd7208b787f319027de2464721cdd4413
+# Pattern for archive including 3rd party source generated in build process.
+GNUPG_FILE_ALL="gnupg-w32-2.2.1_*.tar.xz"
+
+# Command line arguments can be used to change default repository and branch.
+for ARG in $* ; do
+    if [ $ARG = "--local" ]; then
+        MAILPILE_GIT="file://$(realpath $(pwd)/../.. )"
+    elif [ $ARG != *- ]; then
+        MAILPILE_BRANCH=$1
+    else
+        echo "Bad command line argument"; exit
+    fi
+    shift
+done
+
+echo " "
+echo "Mailpile repository:  $MAILPILE_GIT"
+echo "Mailpile branch:      $MAILPILE_BRANCH"
+
+# Capture path to script directory, get the enclosing project directory path,
+# define a path for downloads from external projects like GnuPG and OpenSSL.
 set -e
 cd "$(dirname "$0")"
 SCRIPTDIR="$(pwd)"
-PYBINARIES="https://www.mailpile.is/files/build/"
-WORKDIR="/tmp/mailpile-winbuild/"
+PROJECTDIR=$(realpath $SCRIPTDIR/../..)
+echo "Project dir:          $PROJECTDIR"
+echo "Script dir:           $SCRIPTDIR"
+DOWNLOADDIR=$PROJECTDIR/SourceArchive
+echo "Source download dir:  $DOWNLOADDIR"
+PYTHON_DL=$PROJECTDIR/PythonDistFiles
+echo "Python download dir:  $PYTHON_DL"
 
-if [ "$1" = "--clean" ]; then
-    rm -rf "$WORKDIR"
-    shift
-fi
+# Create build directory and source archive directory
+rm -rf /tmp/mailpile-winbuild/*         # Use rm -rf with hardcoded path only!
+WORKDIR="/tmp/mailpile-winbuild"
+mkdir -p $WORKDIR/SourceArchive
+echo "Working dir:          $WORKDIR"
+echo " "
+sleep 5
 
-OUTPUT="Mailpile-Beta-Installer.exe"
-if [ "$1" = "--output" -a "$2" != "" ]; then
-    OUTPUT="$2"
-    shift
-    shift
-fi
-echo "=== Target file is: $OUTPUT"
-
-
-echo '=== Install apps we depend on'
-which makensis >/dev/null || sudo apt-get install nsis
-
-
-echo '=== Fetch / update Mailpile itself'
-mkdir -p $WORKDIR && cd $WORKDIR
-[ -d Mailpile ] || git clone https://github.com/pagekite/Mailpile
-(cd Mailpile && git pull && git checkout -f master)
-
-
-echo '=== Download and extract binary packages'
-rm -rf $WORKDIR/Buildroot && mkdir $WORKDIR/Buildroot
-mkdir -p $WORKDIR/Downloads && cd $WORKDIR/Downloads
-# Maybe later: pywin32-219.win32-py2.7.exe \
-for package in python-2.7.8-win32.zip \
-               openssl-1.0.1h-win32.zip \
-               gnupg-1.4.18-win32.zip \
-               MailpileLauncher.zip \
-               lxml-3.3.5.win32-py2.7.exe \
-               Pillow-2.5.3.win32-py2.7.exe \
-               setuptools-5.4.2.win32-py2.7.exe \
-               pip-1.5.6.win32-py2.7.exe \
-               pyreadline-2.0.win32-py2.7.exe \
-               pydns-2.3.6-win32-py2.7.zip \
-               jinja2-2.7.3-win32-py2.7.zip \
-               markdown-2.4.1-win32-py2.7.zip \
-               pgpdump-1.5-win32-py2.7.zip \
-; do
-    if [ "$(hostname)" = "mailpile.is" ]; then
-        cp -f /home/mailpile/www/files/build/$package .
-    else
-        wget -q -c $PYBINARIES/$package || true
-    fi
-    (cd ../Buildroot; unzip -q -o ../Downloads/$package || true)
-done
-
-echo '=== Assemble Mailpile environment'
-cd ../Buildroot
-mkdir -p Python27/Scripts Python27/Lib/site-packages/
-mv -f PLATLIB/* PURELIB/* Python27/Lib/site-packages/ && rmdir PLATLIB PURELIB
-mv -f SCRIPTS/* Python27/Scripts && rmdir SCRIPTS
-rm -rf ../Mailpile/{OpenSSL,GnuPG,Python27,Mailpile.exe,Mailpile.exe.config,img}
-mv OpenSSL GnuPG Python27 MailpileLauncher/MailpileLauncher/* ../Mailpile
-cp -f "$SCRIPTDIR/launcher.exe.config" ../Mailpile/Mailpile.exe.config
-
-echo '=== Build NSIS installer'
-cd ../Mailpile
-makensis -V2 -NOCD "$SCRIPTDIR/mailpile.nsi"
+# Get Mailpile and save an archive --of it.
+cd $WORKDIR
+git clone --branch $MAILPILE_BRANCH --depth=1 --recursive $MAILPILE_GIT Mailpile
+cd Mailpile;
+COMMIT=`git log -1 --pretty=format:"%h"`
 cd ..
-mv -f Mailpile/Mailpile-Installer.exe "$OUTPUT"
-ls -l "$OUTPUT"
+tar -c --file $WORKDIR/SourceArchive/Mailpile-$COMMIT.tar.gz --gzip Mailpile
+cd Mailpile
 
-[ "$1" = "--copy" -a "$2" != "" ] && cp "$OUTPUT" "$2"
-[ "$1" = "--move" -a "$2" != "" ] && mv "$OUTPUT" "$2"
+# Create install directories for Python and its packages
+# Dummy file ensures archive programs will not ignore.
+mkdir -p Python27/Lib/site-packages
+echo " " >  Python27/Lib/site-packages/dummy
+
+# Get the distribution files for Python and all needed packages
+# Then copy them to the installer image.
+
+echo " "
+echo "Get Python"
+mkdir -p $PYTHON_DL; cd $PYTHON_DL
+
+if [ -e $PYTHON_FILE ]; then
+    echo "    Reusing previously downloaded Python MSI file"
+    echo "    To force download, delete $PYTHON_DL/$PYTHON_FILE"
+else
+    echo "    Download Python MSI file"
+    wget -c $PYTHON_SITE/$PYTHON_FILE || true
+fi
+# Test source code integrity.
+if [ `openssl dgst -md5 -hex $PYTHON_FILE | cut -d " " --f 2` != \
+    $PYTHON_FILE_MD5 ]
+then
+    echo "File integrity check failed"
+    exit
+fi
+
+# Get distribution files for Python packages listed in $REQUIREMENTS.
+# First try for a Python 2.7 Win32 .whl file.
+# (Python won't install these on a Linux build system, but will download them.)
+# If none, try for a pure Python package.
+# FIXME: pip downloads latest version even if earlier version is present 
+    echo pypiwin32      # Get pypiwin32 even if it is not in $REQUIREMENTS.
+    python -m pip download --no-cache-dir --no-deps --dest $PYTHON_DL \
+                pypiwin32 \
+                --only-binary :all: --platform win32 --python-version 27
+while read PACKAGE; do
+    # Python MSI file already includes pip and setuptools.
+    if [ $PACKAGE == pip* ] || [ $PACKAGE == setuptools* ]; then continue; fi
+    
+    echo $PACKAGE
+    if ! python -m pip download --no-cache-dir --no-deps --dest $PYTHON_DL \
+                $PACKAGE \
+                --only-binary :all: --platform win32 --python-version 27
+    then
+        python -m pip download --no-cache-dir --no-deps --dest $PYTHON_DL \
+                $PACKAGE --no-binary :all:
+    fi
+done < "$WORKDIR/Mailpile/$REQUIREMENTS"
+
+cp -r $PYTHON_DL -t $WORKDIR/Mailpile
+
+# Get non-Python packages
+
+# Create subdirectory for downloading 3rd party source code
+mkdir -p $DOWNLOADDIR
+
+# MailpileLauncher 
+# FIXME: At present this has to be built externally with MS Visual Studio.
+# FIXME: File integrity check?
+
+echo " "
+echo "Get MailpileLauncher"
+if [ -e $SCRIPTDIR/MailpileLauncher/Mailpile.exe ]; then
+    echo "    Reusing previously downloaded MailpileLauncher"
+    echo "    To force rebuild, delete $SCRIPTDIR/MailpileLauncher"
+else
+    echo "    Downloading MailpileLauncher"
+    cd $DOWNLOADDIR
+    wget -q -c https://www.mailpile.is/files/build/MailpileLauncher.zip ||true
+    cd $SCRIPTDIR
+    rm -rf MailpileLauncher/*
+    unzip $DOWNLOADDIR/MailpileLauncher.zip
+fi
+cp -r $SCRIPTDIR/MailpileLauncher/MailpileLauncher/* -t $WORKDIR/Mailpile
+
+# OpenSSL
+#
+# See ./Configure and ./Makefile in OpenSSL source tree for options and targets.
+# See https://marc.wäckerlin.ch/computer/cross-compile-openssl-for-windows-on-linux
+#
+echo " "
+echo "Get OpenSSL"
+if [ -e $SCRIPTDIR/OpenSSL/bin/openssl.exe ]; then
+    echo "    Reusing previously built OpenSSL"
+    echo "    To force rebuild, delete $SCRIPTDIR/OpenSSL"
+else
+    echo "    Rebuilding OpenSSL"
+    # Download source code archive if not already in the project (-c option).
+    cd $DOWNLOADDIR
+    if [ ! -f $DOWNLOADDIR/$OPEN_SSL ]; then    
+        wget -c $OPENSSL_SITE/$OPENSSL_FILE || true
+    fi
+    # Test source code integrity.
+    if [ `openssl dgst -sha256 -hex $OPENSSL_FILE | cut -d " " --f 2` != \
+        $OPENSSL_FILE_SHA256 ]
+    then
+        echo "File integrity check failed"
+        exit
+    fi
+    
+    # Expand the archive and cd into the OpenSSL project root.
+    cd $WORKDIR
+    rm -rf OpenSSL/* ; mkdir -p OpenSSL ; cd OpenSSL
+    cp $DOWNLOADDIR/$OPENSSL_FILE  ./
+    tar --extract -f $OPENSSL_FILE
+    rm $OPENSSL_FILE
+    cd *
+    
+    # Build OpenSSL
+    # For shared library build use ... mingw shared  ...
+    # For static build use ... mingw no-shared -static ...
+    ./Configure mingw shared --cross-compile-prefix=i686-w64-mingw32- \
+                --prefix=$WORKDIR/OpenSSL
+    make build_programs
+    make install_sw
+    
+    # Copy needed files to script directory.
+    cd $SCRIPTDIR;  rm -rf OpenSSL ; mkdir -p OpenSSL
+    cp -r $WORKDIR/OpenSSL/bin -t $SCRIPTDIR/OpenSSL
+fi
+cp -r $SCRIPTDIR/OpenSSL -t $WORKDIR/Mailpile
+
+# GnuPG
+#
+# See build-aux/speedo.mk for targets and options.
+# See https://wiki.gnupg.org/Build2.1_Windows
+#
+echo " "
+echo "Get GnuPG"
+if [ -e $SCRIPTDIR/GnuPG/gpg.exe ]; then
+    echo "    Reusing previously built GnuPG"
+    echo "    To force rebuild, delete $SCRIPTDIR/GnuPG"
+else
+    echo "    Rebuilding GnuPG"
+    if [ ! -f $DOWNLOADDIR/$GNUPG_FILE_ALL ]; then
+        # Consolidated source archive not in project. Download GnuPG source
+        # archive and 3rd party source archives, build consolidated archive.
+        cd $DOWNLOADDIR
+        wget -c $GNUPG_SITE/$GNUPG_FILE || true
+        # Test source code integrity.
+        if [ `openssl dgst -sha1 -hex $GNUPG_FILE | cut -d " " --f 2` != \
+            $GNUPG_FILE_SHA1 ]
+        then
+            echo "File integrity check failed"
+            exit
+        fi
+        
+        cd $WORKDIR
+        rm -rf GnuPG/* ; mkdir -p GnuPG ; cd GnuPG
+        mv $DOWNLOADDIR/$GNUPG_FILE  ./
+        tar --extract -f $GNUPG_FILE
+        rm $GNUPG_FILE
+        cd *
+        
+        # The w32-source target downloads third party source packages
+        # makes a consolidated archive containing GnuPG and 3rd party source
+        # then builds the .dll and .exe.files       
+        make -f build-aux/speedo.mk w32-source
+        
+        # Save the consolidated source archive for next time.
+        mv $GNUPG_FILE_ALL $DOWNLOADDIR        
+    else
+        # Build from existing consolidated source archive.
+        cd $WORKDIR
+        rm -rf GnuPG/* ; mkdir -p GnuPG ; cd GnuPG
+        cp $DOWNLOADDIR/$GNUPG_FILE_ALL  ./
+        tar --extract -f $GNUPG_FILE_ALL
+        rm $GNUPG_FILE_ALL
+        cd *
+        
+        # The this-w32-source target uses 3rd party source
+        # in the consolidated archive saved from last build.
+        # then builds the .dll and .exe.files.       
+        make -f build-aux/speedo.mk this-w32-source
+    fi
+    
+    # Copy needed files to script directory.
+    cd $SCRIPTDIR;  rm -rf GnuPG ; mkdir -p GnuPG
+    cd $WORKDIR/GnuPG/*
+    cp -r PLAY/inst/bin/* -t $SCRIPTDIR/GnuPG
+fi
+cp -r $SCRIPTDIR/GnuPG -t $WORKDIR/Mailpile
+
+#cd $WORKDIR
+#mkdir STunnel
+#cd STunnel
+#wget -q -c https://www.stunnel.org/downloads/stunnel-5.42-win32-installer.exe || true
+#7z e -aoa stunnel-5.42-win32-installer.exe
+#cd ..
+#mv STunnel Mailpile
+
+cd $WORKDIR/Mailpile
+
+# Build the installer
+
+makensis -V2 -NOCD "$SCRIPTDIR/mailpile.nsi"
+
+# Save source code archive to publish for licence compliance.
+cp -r $DOWNLOADDIR/* -t $WORKDIR/SourceArchive
+
+echo " "
+echo "Windows installer:    $WORKDIR/Mailpile-Installer.exe"
+echo "Source code archive:  $WORKDIR/SourceArchive"
+echo " "
+sleep 5
+
+
+      
+
+
+
+
+
