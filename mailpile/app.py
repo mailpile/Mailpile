6a1b12b475bcbe02828e9999d607c8b6ee7f1590
Jack Dodds <brmdamon@hushmail.com>
2018-01-30 18:32:45 -0500

    Delete debug print

--- mailpile/app.py
+++ mailpile/app.py
@@ -187,11 +187,9 @@ def Main(args):
         session.main = True
         try:
             CatchUnixSignals(session)
-            config.clean_tempfile_dir()     # *** What if this is 2nd Mailpile.
+            config.clean_tempfile_dir()
             config.load(session)
-            print 'TTTTT'
         except IOError:
-            print 'UUUUU', config.sys.lockdown
             if config.sys.debug:
                 session.ui.error(_('Failed to decrypt configuration, '
                                    'please log in!'))
@@ -200,8 +198,6 @@ def Main(args):
     except AccessError, e:
         session.ui.error('Access denied: %s\n' % e)
         sys.exit(1)
-
-    print 'AAAAA', config.sys.lockdown
 
     try:
         try:
@@ -248,11 +244,6 @@ def Main(args):
         traceback.print_exc()
 
     finally:
-    
-        print 'BBBBB',config.sys.lockdown
-        if isinstance(config.lock_workdir, str):
-            pass
-    
         if readline is not None:
             readline.write_history_file(session.config.history_file())
 
