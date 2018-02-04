9a70f43ee8b74d788e906b6c1da878c91ca501e9
Bjarni R. Einarsson <bre@klaki.net>
2018-02-04 10:03:28 +0100

    Remove very obsolete mentions of the Apache 2.0 license

--- shared-data/default-theme/js/libraries.js
+++ shared-data/default-theme/js/libraries.js
@@ -1,4 +1,4 @@
 /* Mailpile libraries.js 0.4.1
  * https://github.com/pagekite/mailpile
- * Mailpile and other contributors; Licensed AGPL3 / Apache 2.0
+ * Mailpile and other contributors; Licensed AGPL3
  */
