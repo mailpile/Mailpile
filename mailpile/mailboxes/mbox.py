Fix off-by-one errors in mailboxes.mbox, add tests

mailpile/mailboxes/mbox.py
@@ -1,11 +1,11 @@
 import errno
 import mailbox
 import os
+import re
 import threading
 import time
 import traceback
 
-import mailpile.mailboxes
 from mailpile.i18n import gettext as _
 from mailpile.i18n import ngettext as _n
 from mailpile.index.mailboxes import MailboxIndex
@@ -19,6 +19,8 @@ class MboxIndex(MailboxIndex):
 
 class MailpileMailbox(mailbox.mbox):
     """A mbox class that supports pickling and a few mailpile specifics."""
+    RE_STATUS = re.compile(
+        '^(X-)?Status:\s*\S+', flags=re.IGNORECASE|re.MULTILINE)
 
     @classmethod
     def parse_path(cls, config, fn, create=False):
@@ -100,9 +102,16 @@ class MailpileMailbox(mailbox.mbox):
     def last_updated(self):
         return self._last_updated
 
+    def keys(self):
+        self.update_toc()
+        return mailbox.mbox.keys(self)
+
+    def toc_values(self):
+        self.update_toc()
+        return self._toc.values()
+
     def update_toc(self):
         fd = self._get_fd()
-
         fd.seek(0, 2)
         cur_length = fd.tell()
         cur_mtime = os.path.getmtime(self._path)
@@ -118,6 +127,7 @@ class MailpileMailbox(mailbox.mbox):
             self._next_key = 0
             self._toc = {}
             start = None
+            len_nl = 1
             while True:
                 self._last_updated = time.time()
                 line_pos = fd.tell()
@@ -130,10 +140,11 @@ class MailpileMailbox(mailbox.mbox):
                     start = line_pos
                 elif line == '':
                     if (start is not None) and (start != line_pos):
-                        self._toc[self._next_key] = (start, line_pos)
+                        self._toc[self._next_key] = (start, line_pos - len_nl)
                         self._next_key += 1
                     break
 
+            self._file = fd
             self._file_length = fd.tell()
             self._mtime = cur_mtime
         self.save(None)
@@ -197,7 +208,11 @@ class MailpileMailbox(mailbox.mbox):
     def get_msg_size(self, toc_id):
         try:
             with self._lock:
-                return self._toc[toc_id][1] - self._toc[toc_id][0]
+                # Note: This is 1 byte less than the TOC measures, because
+                #       the final newline is ommitted. The From line is
+                #       included though.
+                start, stop = self._toc[toc_id]
+                return (stop - start)
         except (IndexError, KeyError, IndexError, TypeError):
             return 0
 
@@ -214,21 +229,25 @@ class MailpileMailbox(mailbox.mbox):
                 self._index = MboxIndex(config, self, mbx_mid=mbx_mid)
         return self._index
 
-    def get_msg_cs(self, start, cs_size, max_length):
+    def get_msg_cs(self, start, cs_size, max_length, chars=4):
+        """Generate a checksum of a given length, ignoring Status headers."""
         with self._lock:
             if start is None:
-                raise IOError(_('No data found'))
+                raise IOError('No data found (start=None)')
             fd = self._file
             fd.seek(start, 0)
             firstKB = fd.read(min(cs_size, max_length))
             if firstKB == '':
-                raise IOError(_('No data found'))
-            return b64w(sha1b64(firstKB)[:4])
+                raise IOError('No data found at %s:%s' % (start, max_length))
+            return b64w(sha1b64(
+                re.sub(self.RE_STATUS, 'Status: ?', firstKB))[:chars])
 
-    def get_msg_cs1k(self, start, max_length):
-        return self.get_msg_cs(start, 1024, max_length)
+    def get_msg_cs4k(self, start, max_length):
+        """A 48-bit (6*8) checksum of the first 4k of message data."""
+        return self.get_msg_cs(start, 4096, max_length, chars=8)
 
     def get_msg_cs80b(self, start, max_length):
+        """A 24-bit (6*4) checksum of the first 80 bytes of message data."""
         return self.get_msg_cs(start, 80, max_length)
 
     def get_msg_ptr(self, mboxid, toc_id):
@@ -238,7 +257,7 @@ class MailpileMailbox(mailbox.mbox):
             return '%s%s:%s:%s' % (mboxid,
                                    b36(msg_start),
                                    b36(msg_size),
-                                   self.get_msg_cs80b(msg_start, msg_size))
+                                   self.get_msg_cs4k(msg_start, msg_size))
 
     def _parse_ptr(self, msg_ptr):
         parts = msg_ptr[MBX_ID_LEN:].split(':')
@@ -246,22 +265,67 @@ class MailpileMailbox(mailbox.mbox):
         length = int(parts[1], 36)
         return parts, start, length
 
-    def get_file_by_ptr(self, msg_ptr):
-        parts, start, length = self._parse_ptr(msg_ptr)
-
+    def _verify_ptr_checksums(self, msg_ptr, start, ignored_fd):
+        """Check whether the msg_ptr checksums match the data at [start]."""
         with self._lock:
-            # Make sure we can actually read the message
+            parts, ignored_start, length = self._parse_ptr(msg_ptr)
             cs80b = self.get_msg_cs80b(start, length)
             if len(parts) > 2:
-                cs1k = self.get_msg_cs1k(start, length)
-                cs = parts[2][:4]
-                if (cs1k != cs and cs80b != cs):
-                    raise IOError(_('Message not found'))
+                cs4k = self.get_msg_cs4k(start, length)
+                cs = parts[2]
+                if (cs4k != cs and cs80b != cs):
+                    return False
+        return True
 
-        # We duplicate the file descriptor here, in case other threads are
-        # accessing the same mailbox and moving it around, or in case we have
-        # multiple PartialFile objects in flight at once.
-        return mailbox._PartialFile(self._get_fd(), start, start + length)
+    def _possible_message_locations(self, msg_ptr, max_locations=15):
+        """Yield possible locations for messages of a given size."""
+        with self._lock:
+            parts, pstart, length = self._parse_ptr(msg_ptr)
+
+            # This is where it is SUPPOSED to be, always check that first.
+            starts = [pstart]
+
+            # Extend the list with other messages of the right size.
+            # We accept two lengths, because there were off-by-one errors
+            # in older versions of Mailpile. :-(
+            starts.extend(sorted([
+                b for b, e in self.toc_values()
+                if length in (e-b, e-b+1) and b != pstart]))
+
+#       print 'TOC: %s' % self._toc
+#       print 'GAPS: %s' % [(b, e-b+1) for b, e in self._toc.values()]
+#       print 'STARTS(%s): %s@%s' % (msg_ptr, length, starts)
+
+        # Yield up to max_locations positions
+        for i, start in enumerate(starts[:max_locations]):
+            yield (start, length)
+
+    def _get_SSLP_by_ptr(self, msg_ptr, verifier=None, from_=False):
+        if verifier is None:
+            verifier = self._verify_ptr_checksums
+        tries = []
+        length = None
+        for from_start, length in self._possible_message_locations(msg_ptr):
+            # We duplicate the file descriptor here, in case other threads
+            # are accessing the same mailbox and moving it around, or in
+            # case we have multiple PartialFile objects in flight at once.
+            tries.append(str(from_start))
+            try:
+                start = from_start
+                stop = from_start + length
+                fd = self._get_fd()
+                if not from_:
+                    fd.seek(start)
+                    length -= len(fd.readline())
+                    start = fd.tell()
+                pf = mailbox._PartialFile(fd, start, stop)
+                if verifier(msg_ptr, from_start, pf):
+                    return (from_start, start, length, pf)
+            except IOError:
+                pass
+        err = '%s: %s %s@%s' % (
+            _('Message not found'), msg_ptr, length, '/'.join(tries))
+        raise IOError(err)
 
     def update(self, *args, **kwargs):
         with self._lock:
@@ -275,21 +339,122 @@ class MailpileMailbox(mailbox.mbox):
         with self._lock:
             return mailbox.mbox.remove(self, *args, **kwargs)
 
+    def get_file_by_ptr(self, msg_ptr, verifier=None, from_=False):
+        with self._lock:
+            from_start, start, length, pfile = self._get_SSLP_by_ptr(
+                msg_ptr, verifier=verifier, from_=from_)
+        return pfile
+
     def remove_by_ptr(self, msg_ptr):
         with self._lock:
-            parts, start, length = self._parse_ptr(msg_ptr)
-            keys = [k for k in self._toc if self._toc[k][0] == start]
+            from_start, start, length, pfile = self._get_SSLP_by_ptr(msg_ptr)
+            keys = [k for k in self._toc if self._toc[k][0] == from_start]
             if keys:
                 return self.remove(keys[0])
         raise KeyError('Not found: %s' % msg_ptr)
 
-    def get_bytes(self, toc_id, *args):
+    def get_bytes(self, toc_id, *args, **kwargs):
         with self._lock:
-            return self.get_file(toc_id).read(*args)
+            return self.get_file(toc_id, *args, **kwargs).read()
 
     def get_file(self, *args, **kwargs):
         with self._lock:
             return mailbox.mbox.get_file(self, *args, **kwargs)
 
 
-mailpile.mailboxes.register(90, MailpileMailbox)
+if __name__ == "__main__":
+    import tempfile, time, sys
+    verbose = ('-v' in sys.argv) or ('--verbose' in sys.argv)
+    wait = ('-w' in sys.argv) or ('--wait' in sys.argv)
+
+    MSG_TEMPLATE = """\
+From bre@mailpile.is  Mon Jan  1 08:14:00 2018
+Return-Path: <bre@mailpile.is>
+Subject: %(subject)s
+Message-ID: <%(msgid)s>
+Content-Length: %(length)s
+
+%(content)s"""
+
+    problems = tests = 0
+    with tempfile.NamedTemporaryFile() as tf:
+        lengths = []
+        for count in range(0, 35):
+             body = ''.join([
+                 'Hello world, this is a message!\n'
+                 ] * ((27 * (100-count)) % 1230))
+             message = (MSG_TEMPLATE % {
+                 'subject': 'Test message #%d' % count,
+                 'msgid': '%d@example.com' % count,
+                 'length': len(body),
+                 'content': body})
+             lengths.append(len(message))
+             tf.write(message)
+             tf.write("\n")
+        tf.flush()
+        if verbose or wait:
+            print 'Temporary mailbox in: %s' % tf.name
+        if wait:
+            raw_input('Press ENTER to continue...')
+
+        pmbx = mailbox.mbox(tf.name)
+        mmbx = MailpileMailbox(tf.name)
+        ptrs = []
+        for i, key in enumerate(mmbx.keys()):
+             msg_ptr = mmbx.get_msg_ptr('0000', key)
+             o_size = lengths[i]
+             c_size = mmbx.get_msg_size(key)
+             f_size = len(mmbx.get_bytes(key, from_=True))
+             f2size = len(mmbx.get_file_by_ptr(msg_ptr, from_=True).read())
+             result = 'ok' if (o_size == c_size == f_size == f2size) else 'BAD'
+             if verbose or result != 'ok':
+                 print "%-3.3s [%s/%s/%s] %s ?= %s ?= %s ?= %s" % (
+                     result, i, key, msg_ptr, o_size, c_size, f_size, f2size)
+             if result != 'ok':
+                 problems += 1
+             tests += 1
+             ptrs.append([msg_ptr, f2size])
+
+        # Remove some messages, bypassing MailpileMailbox
+        deletions = [0, 5, 10, 15, 34]
+        for d in reversed(sorted(deletions)):
+            del pmbx[d]
+        pmbx.flush()
+
+        # Remove a message using MailpileMailbox
+        try:
+            tests += 1
+            deletions.append(1)
+            mmbx.remove_by_ptr(ptrs[1][0])
+            mmbx.flush()
+        except KeyError:
+            problems += 1
+
+        for i, (msg_ptr, f2size) in enumerate(ptrs):
+            tests += 1
+            if i in deletions:
+                try:
+                    mmbx.get_file_by_ptr(msg_ptr, from_=True).read()
+                    problems += 1
+                    print('BAD Found deleted message %s' % msg_ptr)
+                except IOError:
+                    if verbose:
+                        print('ok  IOError on message %s' % msg_ptr)
+                continue
+            f3size = len(mmbx.get_file_by_ptr(msg_ptr, from_=True).read())
+            if (f2size != f3size):
+                problems += 1
+                print('BAD Message %s: wrong size in new location' % msg_ptr)
+            elif verbose:
+                print('ok  Message %s found in new location' % msg_ptr)
+
+        # This is formatted to look like doctest results...
+        print 'TestResults(failed=%d, attempted=%d)' % (problems, tests)
+        if wait:
+            raw_input('Tests finished. Press ENTER to clean up...')
+
+    if problems:
+        sys.exit(1)
+else:
+    import mailpile.mailboxes
+    mailpile.mailboxes.register(90, MailpileMailbox)