c32d813651a57d7a819089133ed5169a29290b85
Alexander Haase <alex@alexanderhaase.net>
2018-08-15 13:35:42 -0400

    Fixed cache url updating to properly handle comments.

--- packages/windows-wix/provide/cache.py
+++ packages/windows-wix/provide/cache.py
@@ -173,22 +173,19 @@ class SemanticCache(object):
         entry = self.resources[name]
         return self.cache.resolve(url = entry['url'], sha1 = entry['sha1'])
 
-    def insert(self, resources):
+    def insert(self, key, url, comment = None):
         '''
-        insert one or more resources as name-url pairs.
-
-        hash is computed on insert.
+        insert a resource--hash is computed on insert.
         '''
-        for key, url, comment in resources.items():
-            path, sha1 = self.cache.insert(url)
-            entry = {'url': url, 'sha1': sha1}
-            if comment:
-                entry['comment'] = comment
-            try:
-                self.resources[key].update(entry)
-            except KeyError:
-                self.resources[key] = entry
-            self.resources[key] = {'url': url, 'sha1': sha1}
+        path, sha1 = self.cache.insert(url)
+        entry = {'url': url, 'sha1': sha1}
+        if comment:
+            entry['comment'] = comment
+        try:
+            self.resources[key].update(entry)
+        except KeyError:
+            self.resources[key] = entry
+        self.resources[key] = {'url': url, 'sha1': sha1}
 
     def save(self, path, indent=2):
         '''
@@ -223,7 +220,7 @@ if __name__ == '__main__':
     parser.add_argument('-r', '--resource', type=str, help="file to lookup")
     parser.add_argument('-l', '--log-level', type=str, help="logging level")
     parser.add_argument('-i', '--insert', type=str,
-                        help="url to insert as {key:value}")
+                        help='url to insert as ["key", "url", "comment"]')
     parser.add_argument('-a', '--all', action='store_true',
                         help='fetch all resources')
     args = parser.parse_args()
@@ -234,7 +231,7 @@ if __name__ == '__main__':
     cache = SemanticCache.load(args.json, Cache(args.cache))
 
     if args.insert:
-        cache.insert(json.loads(args.insert))
+        cache.insert(*json.loads(args.insert))
         cache.save(args.json)
 
     if args.resource:
