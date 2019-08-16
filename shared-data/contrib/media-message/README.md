7ebb1ad2a26765afb84238d8772cfa3c24b98166
Bjarni R. Einarsson <bre@klaki.net>
2019-08-16 17:20:42 +0000

    Removing dead and unused plugins

--- shared-data/contrib/media-message/README.md
+++
@@ -1,6 +0,0 @@
-Media Message
-=============
-
-A plugin that will one day make it dead easy to attach video, audio, or pictures messages to email using your webcam and microphone!
-
-Interested in helping develop this? See this [Github Issue](https://github.com/pagekite/Mailpile/issues/259)
--- shared-data/contrib/sms/twilio_sms.py
+++
@@ -1,8 +0,0 @@
-from twilio.rest import TwilioRestClient
-
-account = "AC######################"
-token = "***************************"
-client = TwilioRestClient(account, token)
-
-message = client.messages.create(to="+15031112222", from_="+13101112222",
-                                 body="Hello there!")
